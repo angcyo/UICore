af1adeedff7a440ba8e1b6b5fc36fd32aa34eba7
angcyo <angcyo@126.com>
2020-01-03 10:36:26 +0800

    * fix toast support activity

--- core/src/main/java/com/angcyo/core/component/DslToast.kt
+++ core/src/main/java/com/angcyo/core/component/DslToast.kt
@@ -1,11 +1,9 @@
 package com.angcyo.core.component
 
+import android.app.Activity
 import android.content.Context
 import android.os.Build
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
+import android.view.*
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
@@ -14,6 +12,7 @@ import androidx.annotation.DrawableRes
 import com.angcyo.core.R
 import com.angcyo.drawable.dpi
 import com.angcyo.drawable.getDimen
+import com.angcyo.drawable.getStatusBarHeight
 import com.angcyo.library.app
 import com.angcyo.library.ex.undefined_int
 import com.angcyo.library.ex.undefined_res
@@ -29,12 +28,22 @@ import java.lang.ref.WeakReference
  * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
  */
 object DslToast {
-    var _toastRef: WeakReference<Toast>? = null
 
     fun show(context: Context = app(), action: ToastConfig.() -> Unit) {
         val config = ToastConfig()
         config.action()
 
+        if (config.activity == null) {
+            _showWithToast(context, config)
+        } else {
+            _showWithActivity(config)
+        }
+    }
+
+    var _toastRef: WeakReference<Toast>? = null
+
+    /**使用[Toast]展示提示信息*/
+    fun _showWithToast(context: Context, config: ToastConfig) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
             _toastRef?.get()?.cancel()
             _toastRef = null
@@ -61,29 +70,7 @@ object DslToast {
                 //没有自定义的布局
                 setText(config.text)
             } else {
-                val layout = LayoutInflater.from(context)
-                    .inflate(config.layoutId, FrameLayout(context), false)
-
-                layout.findViewById<TextView>(R.id.lib_text_view)?.apply {
-                    if (config.text.isEmpty()) {
-                        visibility = View.GONE
-                    } else {
-                        visibility = View.VISIBLE
-                        this.text = config.text
-                    }
-                }
-                layout.findViewById<ImageView>(R.id.lib_image_view)?.apply {
-                    if (config.icon == undefined_res) {
-                        visibility = View.GONE
-                    } else {
-                        visibility = View.VISIBLE
-                        setImageResource(config.icon)
-                    }
-                }
-
-                config.onBindView(layout)
-
-                view = layout
+                view = _inflateLayout(context, config)
             }
 
             show()
@@ -107,17 +94,157 @@ object DslToast {
             e.printStackTrace()
         }
     }
+
+    var _viewRef: WeakReference<View>? = null
+    var _lastViewTag = mutableListOf<Int>()
+
+    fun _showWithActivity(config: ToastConfig) {
+
+        _viewRef?.get()?.let { rootView ->
+            (rootView.parent as? ViewGroup)?.let { viewGroup ->
+                when (config.removeLastView) {
+                    1 -> {
+                        for (tag in _lastViewTag) {
+                            val view: View? = viewGroup.findViewWithTag(tag)
+                            view?.run { viewGroup.removeView(this) }
+                        }
+                        _lastViewTag.clear()
+                        _viewRef = null
+                    }
+                    0 -> {
+                        val view: View? = viewGroup.findViewWithTag(config.layoutId)
+                        view?.run { viewGroup.removeView(this) }
+                        _lastViewTag.remove(config.layoutId)
+                        _viewRef = null
+                    }
+                    else -> {
+                        //no op
+                    }
+                }
+            }
+        }
+
+        if (config.layoutId == undefined_int) {
+            return
+        }
+
+        //已经存在相同布局
+        val tag = _viewRef?.get()?.tag
+        if (tag != null && tag != config.layoutId) {
+            _viewRef?.get()?.run {
+                _removeRunnable(this)
+                _initLayout(this, config)
+                _hideTagView(this, config.duration)
+                _lastViewTag.add(config.layoutId)
+            }
+            return
+        }
+
+        //创建新的布局
+        config.activity?.apply {
+            val contentLayout: FrameLayout? =
+                window.findViewById(Window.ID_ANDROID_CONTENT)
+
+            val layout = _inflateLayout(this, config)
+
+            layout.tag = config.layoutId
+
+            if (config.fullScreen) {
+                contentLayout?.addView(layout, FrameLayout.LayoutParams(-1, -2).apply {
+                    if (config.yOffset > 0) {
+                        topMargin = config.yOffset + getStatusBarHeight()
+                    }
+                    leftMargin = config.fullMargin
+                    rightMargin = config.fullMargin
+                    gravity = config.gravity
+                })
+            } else {
+                contentLayout?.addView(layout)
+            }
+
+            _viewRef = WeakReference(layout)
+
+            _hideTagView(layout, config.duration)
+            _lastViewTag.add(config.layoutId)
+        }
+    }
+
+    /**移除[view]*/
+    fun _removeView(view: View) {
+        (view.parent as? ViewGroup)?.let { viewGroup ->
+            viewGroup.removeView(view)
+            view.tag?.let {
+                if (it is Int) {
+                    _lastViewTag.remove(it)
+                }
+            }
+        }
+    }
+
+    /**自动隐藏[view]*/
+    fun _hideTagView(view: View, duration: Int) {
+        _removeRunnable(view)
+        val runnable = Runnable {
+            _removeView(view)
+        }
+        if (duration == Toast.LENGTH_LONG) {
+            view.postDelayed(runnable, 7000)
+        } else {
+            view.postDelayed(runnable, 4000)
+        }
+    }
+
+    /**移除[view]的自动隐藏[Runnable]*/
+    fun _removeRunnable(view: View) {
+        val tag = view.getTag(R.id.tag)
+        if (tag is Runnable) {
+            view.removeCallbacks(tag)
+            view.setTag(R.id.tag, null)
+        }
+    }
+
+    fun _inflateLayout(context: Context, config: ToastConfig): View {
+        val layout = LayoutInflater.from(context)
+            .inflate(config.layoutId, FrameLayout(context), false)
+
+        _initLayout(layout, config)
+
+        return layout
+    }
+
+    fun _initLayout(layout: View, config: ToastConfig) {
+        layout.findViewById<TextView>(R.id.lib_text_view)?.apply {
+            if (config.text.isEmpty()) {
+                visibility = View.GONE
+            } else {
+                visibility = View.VISIBLE
+                this.text = config.text
+            }
+        }
+        layout.findViewById<ImageView>(R.id.lib_image_view)?.apply {
+            if (config.icon == undefined_res) {
+                visibility = View.GONE
+            } else {
+                visibility = View.VISIBLE
+                setImageResource(config.icon)
+            }
+        }
+
+        config.onBindView(layout)
+    }
 }
 
 data class ToastConfig(
-    var duration: Int = Toast.LENGTH_LONG,
+    var activity: Activity? = null, //附着在Activity上, 不用toast展示
+    var removeLastView: Int = 0,//1:移除 0:移除相同layoutId -1:不移除
+    var duration: Int = Toast.LENGTH_SHORT,
     var text: CharSequence = "",
     @DrawableRes
     var icon: Int = undefined_res,
     var layoutId: Int = undefined_int,
     var gravity: Int = Gravity.CENTER_HORIZONTAL or Gravity.TOP,
     var xOffset: Int = 0,
-    var yOffset: Int = getDimen(R.dimen.action_bar_height) + 10 * dpi, //从状态栏的底部开始算
+    var yOffset: Int = getDimen(R.dimen.action_bar_height) + 10 * dpi, //Toast模式下,从状态栏的底部开始算
     var fullScreen: Boolean = true,//全屏模式
     var fullMargin: Int = 20 * dpi,//全屏模式下, 宽度左右的margin
 
@@ -128,10 +255,17 @@ fun toast(action: ToastConfig.() -> Unit) {
     DslToast.show(action = action)
 }
 
-fun toast(text: CharSequence?, @DrawableRes icon: Int = undefined_res) {
+fun toast(
+    text: CharSequence?,
+    @DrawableRes
+    icon: Int = undefined_res,
+    action: ToastConfig.() -> Unit = {}
+) {
     DslToast.show {
         layoutId = R.layout.lib_toast_layout
         this.text = text ?: ""
         this.icon = icon
+
+        this.action()
     }
 }
