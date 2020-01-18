f3b463976a3bbe2daa0b911107d0dde036177d6d
angcyo <angcyo@126.com>
2020-01-18 22:30:38 +0800

    + bundle ex , fix base

---
+++ fragment/src/main/java/com/angcyo/BundleEx.kt
@@ -0,0 +1,97 @@
+package com.angcyo
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.angcyo.http.base.fromJson
+import com.angcyo.http.base.toJson
+
+/**
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/01/18
+ */
+
+//<editor-fold desc="Bundle操作">
+
+const val BUNDLE_KEY_JSON = "BUNDLE_KEY_JSON"
+
+/**创建一个默认传输[json]数据的[Bundle]*/
+fun jsonBundle(data: Any?, key: String = BUNDLE_KEY_JSON): Bundle {
+    val bundle = Bundle()
+    bundle.putData(data, key)
+    return bundle
+}
+
+/**获取[Bundle]中传输的[json]数据*/
+fun Bundle.getJson(key: String = BUNDLE_KEY_JSON) = getString(key)
+
+fun Bundle.putData(data: Any?, key: String = BUNDLE_KEY_JSON) {
+    when (data) {
+        is String -> putString(key, data)
+        is Number -> putString(key, data.toString())
+        else -> putString(key, data.toJson())
+    }
+}
+
+inline fun <reified DATA> Bundle.getData(key: String = BUNDLE_KEY_JSON): DATA? =
+    getJson(key)?.covert()
+
+//</editor-fold desc="Bundle操作">
+
+/**将[json]转换成对应的数据类型*/
+inline fun <reified DATA> String.covert(): DATA? {
+    val cls = DATA::class.java
+    return when {
+        cls.isAssignableFrom(String::class.java) -> this as? DATA
+        cls.isAssignableFrom(Int::class.java) -> this.toInt() as? DATA
+        cls.isAssignableFrom(Long::class.java) -> this.toLong() as? DATA
+        cls.isAssignableFrom(Float::class.java) -> this.toFloat() as? DATA
+        cls.isAssignableFrom(Double::class.java) -> this.toDouble() as? DATA
+        else -> this.fromJson(cls)
+    }
+}
+
+//<editor-fold desc="Fragment put get">
+
+/**快速设置[Fragment]的参数, 如果已经存在[arguments], 则追加数据*/
+fun Fragment.putData(data: Any?, key: String = BUNDLE_KEY_JSON) {
+    val bundle = jsonBundle(data, key)
+
+    arguments?.putAll(bundle)
+
+    if (arguments == null) {
+        arguments = bundle
+    }
+}
+
+/**快速从[Fragment]获取[putData]设置的数据*/
+inline fun <reified DATA> Fragment.getData(key: String = BUNDLE_KEY_JSON): DATA? =
+    arguments?.getJson(key)?.covert()
+
+//</editor-fold desc="Fragment put get">
+
+//<editor-fold desc="Intent put get">
+
+fun Intent.putData(data: Any?, key: String = BUNDLE_KEY_JSON) {
+    extras?.putData(data, key)
+}
+
+inline fun <reified DATA> Intent.getData(key: String = BUNDLE_KEY_JSON): DATA? =
+    extras?.getData(key)
+
+//</editor-fold desc="Intent put get">
+
+//<editor-fold desc="Activity put get">
+
+fun Activity.putData(data: Any?, key: String = BUNDLE_KEY_JSON) {
+    intent.putData(data, key)
+}
+
+inline fun <reified DATA> Activity.getData(key: String = BUNDLE_KEY_JSON): DATA? =
+    intent.getData(key)
+
+//</editor-fold desc="Activity put get">
+
--- fragment/src/main/java/com/angcyo/DslAHelper.kt
+++ fragment/src/main/java/com/angcyo/DslAHelper.kt
@@ -3,6 +3,15 @@ package com.angcyo
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.transition.Fade
+import android.transition.Transition
+import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
+import androidx.core.view.ViewCompat
 import com.angcyo.library.L
 
 /**
@@ -15,19 +24,23 @@ import com.angcyo.library.L
 class DslAHelper(val context: Context) {
 
     /**需要启动的[Intent]*/
-    val startIntent = mutableListOf<Intent>()
+    val startIntentConfig = mutableListOf<IntentConfig>()
+
+    /**关闭自身*/
+    var finishSelf: Boolean = false
 
     //<editor-fold desc="start操作">
 
-    fun start(intent: Intent, action: Intent.() -> Unit = {}) {
+    fun start(intent: Intent, action: IntentConfig.() -> Unit = {}) {
+        val config = IntentConfig(intent)
         if (context !is Activity) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
-        intent.action()
-        startIntent.add(intent)
+        config.action()
+        startIntentConfig.add(config)
     }
 
-    fun start(aClass: Class<*>, action: Intent.() -> Unit = {}) {
+    fun start(aClass: Class<*>, action: IntentConfig.() -> Unit = {}) {
         val intent = Intent(context, aClass)
         start(intent, action)
     }
@@ -35,13 +48,192 @@ class DslAHelper(val context: Context) {
     //</editor-fold desc="start操作">
 
     fun doIt() {
-        startIntent.forEach {
+        startIntentConfig.forEach {
             try {
-                context.startActivity(it)
+
+                //op
+                if (context !is Activity) {
+                    it.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+
+                //共享元素配置
+                var transitionOptions: Bundle? = null
+                if (windowEnterTransition != null ||
+                    windowExitTransition != null ||
+                    it.sharedElementList.isNotEmpty()
+                ) {
+                    _supportTransition {
+                        transitionOptions = if (it.sharedElementList.isNotEmpty()) {
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                (context as Activity),
+                                *it.sharedElementList.toTypedArray()
+                            ).toBundle()
+                        } else {
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(context as Activity)
+                                .toBundle()
+                        }
+
+                        //https://developer.android.com/training/transitions/start-activity#custom-trans
+                        context.window.apply {
+                            //requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+                            //requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+                        }
+                    }
+                }
+
+                //ForResult
+                if (it.requestCode != -1 && context is Activity) {
+                    ActivityCompat.startActivityForResult(
+                        context,
+                        it.intent,
+                        it.requestCode,
+                        transitionOptions
+                    )
+                } else {
+                    ActivityCompat.startActivity(
+                        context,
+                        it.intent,
+                        transitionOptions
+                    )
+                }
+
+                //anim
+                if (context is Activity) {
+                    if (it.enterAnim != -1 || it.exitAnim != -1) {
+                        context.overridePendingTransition(it.enterAnim, it.exitAnim)
+                    }
+                }
             } catch (e: Exception) {
                 L.e("启动Activity失败:$e")
             }
         }
+
+        if (finishSelf && context is Activity) {
+            context.onBackPressed()
+        }
     }
 
+    //<editor-fold desc="finish 操作">
+
+    /**关闭当前[context]*/
+    open fun finish(withBackPress: Boolean = false, action: IntentConfig.() -> Unit = {}) {
+        if (context is Activity) {
+            val config = IntentConfig(Intent())
+            context.setResult(config.resultCode, config.resultData)
+
+            config.action()
+
+            if (withBackPress) {
+                context.onBackPressed()
+            } else {
+                context.finish()
+            }
+            if (config.enterAnim != -1 || config.exitAnim != -1) {
+                context.overridePendingTransition(config.enterAnim, config.exitAnim)
+            }
+        } else {
+            L.e("context 必须是 Activity, 才能执行 finish()")
+        }
+    }
+
+    //</editor-fold desc="finish 操作">
+
+    //<editor-fold desc="转场动画配置">
+
+    //https://developer.android.com/training/transitions/start-activity#custom-trans
+
+    /**
+     * 转场动画支持.
+     * 步骤1: 获取共享元素属性值
+     * 步骤2: 传递属性
+     * 步骤3: 播放动画
+     */
+
+    //是否支持转场动画
+    fun _supportTransition(action: () -> Unit) {
+        if (context is Activity &&
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+        ) {
+            action()
+        }
+    }
+
+    //</editor-fold desc="转场动画配置">
+
+    //<editor-fold desc="启动转场动画配置">
+
+    //可以单独设置窗口的过渡动画, 而无需设置共享元素
+    var windowExitTransition: Transition? = Fade()
+    var windowEnterTransition: Transition? = Fade()
+
+    var elementEnterTransition: Transition? = null
+    var elementExitTransition: Transition? = null
+
+    /**启动[Activity]后, 调用此方法开始转场动画*/
+    fun transition(action: IntentConfig.() -> Unit = {}) {
+
+        _supportTransition {
+            if (context is Activity) {
+                context.window.apply {
+                    //requestFeature(Window.FEATURE_CONTENT_TRANSITIONS)
+                    //requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
+
+                    enterTransition = windowEnterTransition
+                    exitTransition = windowExitTransition
+
+                    sharedElementEnterTransition = elementEnterTransition
+                    sharedElementExitTransition = elementExitTransition
+                }
+            }
+        }
+
+        val config = IntentConfig(Intent())
+
+        for (pair in config.sharedElementList) {
+            pair.first?.run { ViewCompat.setTransitionName(this, pair.second) }
+        }
+    }
+
+    //</editor-fold desc="启动转场动画配置">
+}
+
+data class IntentConfig(
+    var intent: Intent,
+
+    //-1 默认动画, 0无动画
+    var enterAnim: Int = -1,
+    var exitAnim: Int = -1,
+
+    //ForResult
+    var requestCode: Int = -1,
+
+    //Result 只在 finish 操作有效
+    var resultCode: Int = Activity.RESULT_CANCELED,
+    var resultData: Intent? = null,
+
+    //共享元素
+    val sharedElementList: MutableList<Pair<View, String>> = mutableListOf()
+)
+
+/**去掉系统默认的动画*/
+fun IntentConfig.noAnim() {
+    enterAnim = 0
+    exitAnim = 0
+}
+
+/**传递Json数据*/
+fun IntentConfig.putData(data: Any?, key: String = BUNDLE_KEY_JSON) {
+    intent.putData(data, key)
+}
+
+/**设置共享元素[View], 和对应的[Key]*/
+fun IntentConfig.transition(sharedElement: View, sharedElementName: String? = null) {
+    sharedElementList.add(
+        Pair(
+            sharedElement,
+            sharedElementName
+                ?: ViewCompat.getTransitionName(sharedElement)
+                ?: sharedElement.javaClass.name
+        )
+    )
 }
