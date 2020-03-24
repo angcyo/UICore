371c54acbe7462cb63be3e0d2ba6b9c37950c80d
angcyo <angcyo@126.com>
2020-03-24 16:37:06 +0800

    * support attr is circle image

--- widget/src/main/java/com/angcyo/widget/image/ShapeImageView.kt
+++ widget/src/main/java/com/angcyo/widget/image/ShapeImageView.kt
@@ -12,7 +12,10 @@ import androidx.appcompat.widget.AppCompatImageView
 import com.angcyo.library.ex.dpi
 import com.angcyo.widget.R
 import com.angcyo.widget.base.InvalidateProperty
+import com.angcyo.widget.base.drawHeight
+import com.angcyo.widget.base.drawWidth
 import com.angcyo.widget.base.save
+import kotlin.math.min
 
 /**
  *
@@ -32,6 +35,9 @@ open class ShapeImageView : AppCompatImageView {
 
     /**边框的宽度*/
     var borderWidth: Int by InvalidateProperty(2 * dpi)
+
+    /**是否是圆*/
+    var isCircle: Boolean by InvalidateProperty(false)
 
     /**边框的颜色*/
     var borderColor: Int by InvalidateProperty(Color.WHITE)
@@ -95,6 +101,7 @@ open class ShapeImageView : AppCompatImageView {
         )
         drawBorder = typedArray.getBoolean(R.styleable.ShapeImageView_r_draw_border, drawBorder)
         enableShape = typedArray.getBoolean(R.styleable.ShapeImageView_r_enable_shape, enableShape)
+        isCircle = typedArray.getBoolean(R.styleable.ShapeImageView_r_is_circle, isCircle)
         borderWidth = typedArray.getDimensionPixelOffset(
             R.styleable.ShapeImageView_r_border_width,
             borderWidth
@@ -107,7 +114,21 @@ open class ShapeImageView : AppCompatImageView {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         if (enableShape) {
-            _outlineRect.set(paddingLeft, paddingTop, w - paddingRight, h - paddingBottom)
+            val right = w - paddingRight
+            val bottom = h - paddingBottom
+            if (isCircle) {
+                val centerX = paddingLeft + drawWidth / 2
+                val centerY = paddingTop + drawHeight / 2
+                val radius = min(drawWidth / 2, drawHeight / 2)
+                _outlineRect.set(
+                    centerX - radius,
+                    centerY - radius,
+                    centerX + radius,
+                    centerY + radius
+                )
+            } else {
+                _outlineRect.set(paddingLeft, paddingTop, right, bottom)
+            }
         } else {
             _outlineRect.set(0, 0, w, h)
         }
@@ -119,6 +140,7 @@ open class ShapeImageView : AppCompatImageView {
 
     override fun onDraw(canvas: Canvas) {
         _outlineRectF.set(_outlineRect)
+
         //mask
         if (maskDrawable == null) {
             super.onDraw(canvas)
@@ -138,17 +160,41 @@ open class ShapeImageView : AppCompatImageView {
             paint.strokeWidth = borderWidth.toFloat()
             paint.color = borderColor
             _outlineRectF.inset(paint.strokeWidth / 2, paint.strokeWidth / 2)
-            canvas.drawRoundRect(_outlineRectF, imageRadius.toFloat(), imageRadius.toFloat(), paint)
+            if (isCircle) {
+                canvas.drawCircle(
+                    _outlineRectF.centerX(),
+                    _outlineRectF.centerY(),
+                    _outlineRectF.width() / 2,
+                    paint
+                )
+            } else {
+                canvas.drawRoundRect(
+                    _outlineRectF,
+                    imageRadius.toFloat(),
+                    imageRadius.toFloat(),
+                    paint
+                )
+            }
         }
     }
 
+    /**绘制在mask内的自定义内容*/
     open fun onDrawInMask(canvas: Canvas) {
 
     }
 
     inner class ShapeOutline : ViewOutlineProvider() {
         override fun getOutline(view: View, outline: Outline) {
-            outline.setRoundRect(_outlineRect, imageRadius.toFloat())
+            if (isCircle) {
+                outline.setOval(
+                    _outlineRect.left,
+                    _outlineRect.top,
+                    _outlineRect.right,
+                    _outlineRect.bottom
+                )
+            } else {
+                outline.setRoundRect(_outlineRect, imageRadius.toFloat())
+            }
         }
     }
 }
