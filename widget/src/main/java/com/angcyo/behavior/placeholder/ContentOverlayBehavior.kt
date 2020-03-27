51d582a3ab2e8c394b9e5b367da813e33ec452ca
angcyo <angcyo@126.com>
2020-03-27 19:04:27 +0800

    * fix conent overlay behavior

--- widget/src/main/java/com/angcyo/behavior/placeholder/ContentOverlayBehavior.kt
+++ widget/src/main/java/com/angcyo/behavior/placeholder/ContentOverlayBehavior.kt
@@ -20,13 +20,27 @@ open class ContentOverlayBehavior(
     attrs: AttributeSet? = null
 ) : BaseDependsBehavior<View>(context, attrs) {
 
+    /**标题栏的行为, 用于布局在标题栏bottom里面*/
+    var titleBarPlaceholderBehavior: ITitleBarBehavior? = null
+
+    /**[child]需要排除多少高度*/
+    val excludeHeight get() = titleBarPlaceholderBehavior?.getContentExcludeHeight(this) ?: 0
+
     override fun layoutDependsOn(
         parent: CoordinatorLayout,
         child: View,
         dependency: View
     ): Boolean {
+
+        val behavior = dependency.behavior()
+        behavior?.let {
+            if (it is ITitleBarBehavior) {
+                titleBarPlaceholderBehavior = it
+            }
+        }
+
         super.layoutDependsOn(parent, child, dependency)
-        return dependency.behavior() is ITitleBarBehavior
+        return behavior is ITitleBarBehavior
     }
 
     override fun onDependentViewChanged(
@@ -38,6 +52,35 @@ open class ContentOverlayBehavior(
         val top = dependency.bottom
         child.layout(child.left, top, child.right, top + child.measuredHeight)
         return false
+    }
+
+    override fun onMeasureChild(
+        parent: CoordinatorLayout,
+        child: View,
+        parentWidthMeasureSpec: Int,
+        widthUsed: Int,
+        parentHeightMeasureSpec: Int,
+        heightUsed: Int
+    ): Boolean {
+
+        super.onMeasureChild(
+            parent,
+            child,
+            parentWidthMeasureSpec,
+            widthUsed,
+            parentHeightMeasureSpec,
+            heightUsed
+        )
+
+        parent.onMeasureChild(
+            child,
+            parentWidthMeasureSpec,
+            widthUsed,
+            parentHeightMeasureSpec,
+            heightUsed + excludeHeight
+        )
+
+        return true
     }
 
     override fun onLayoutChild(
