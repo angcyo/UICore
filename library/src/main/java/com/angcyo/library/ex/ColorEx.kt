0d39dbe81ad6934f263e4c6a5df1fa0c6be09aaa
angcyo <angcyo@126.com>
2020-04-09 15:46:39 +0800

    * fix base

--- library/src/main/java/com/angcyo/library/ex/ColorEx.kt
+++ library/src/main/java/com/angcyo/library/ex/ColorEx.kt
@@ -15,14 +15,31 @@ import kotlin.random.Random.Default.nextInt
  * @date 2020/01/20
  */
 
-fun randomColor(random: Random = Random(SystemClock.elapsedRealtime())): Int {
-    return randomColor(random, 120, 250)
+/**随机产生一个颜色*/
+fun randomColor(minValue: Int = 120, maxValue: Int = 250): Int {
+    val r = nextInt(minValue, maxValue)
+    val g = nextInt(minValue, maxValue)
+    val b = nextInt(minValue, maxValue)
+    return Color.rgb(r, g, b)
+}
+
+/**随机产生一个颜色(带透明)*/
+fun randomColorAlpha(minValue: Int = 120, maxValue: Int = 250): Int {
+    val a = nextInt(minValue, maxValue)
+    val r = nextInt(minValue, maxValue)
+    val g = nextInt(minValue, maxValue)
+    val b = nextInt(minValue, maxValue)
+    return Color.argb(a, r, g, b)
+}
+
+fun randomColorIn(random: Random = Random(SystemClock.elapsedRealtime())): Int {
+    return randomColorIn(random, 120, 250)
 }
 
 /**
  * 随机颜色, 设置一个最小值, 设置一个最大值, 第三个值在这2者之间随机改变
  */
-fun randomColor(random: Random, minValue: Int, maxValue: Int): Int {
+fun randomColorIn(random: Random, minValue: Int, maxValue: Int): Int {
     val a = minValue + random.nextInt(maxValue - minValue)
     val list1: MutableList<Int> = ArrayList()
     val list2: MutableList<Int> = ArrayList()
@@ -36,22 +53,8 @@ fun randomColor(random: Random, minValue: Int, maxValue: Int): Int {
     return Color.rgb(list2[0], list2[1], list2[2])
 }
 
-fun randomColor(minValue: Int = 120, maxValue: Int = 250): Int {
-    val r = nextInt(minValue, maxValue)
-    val g = nextInt(minValue, maxValue)
-    val b = nextInt(minValue, maxValue)
-    return Color.rgb(r, g, b)
-}
-
-fun randomColorAlpha(minValue: Int = 120, maxValue: Int = 250): Int {
-    val a = nextInt(minValue, maxValue)
-    val r = nextInt(minValue, maxValue)
-    val g = nextInt(minValue, maxValue)
-    val b = nextInt(minValue, maxValue)
-    return Color.argb(a, r, g, b)
-}
-
 private var argbEvaluator: ArgbEvaluator = ArgbEvaluator()
+
 /**根据比例, 获取评估后的颜色值[fraction][0-1]*/
 fun evaluateColor(fraction: Float, startValue: Int, endValue: Int): Int {
     return argbEvaluator.evaluate(fraction, startValue, endValue) as Int
