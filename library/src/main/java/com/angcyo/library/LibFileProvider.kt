067984e84ff4b60e362f5b892fed2bb936a9f4c1
angcyo <angcyo@126.com>
2020-04-23 17:48:48 +0800

    + provider

---
+++ library/src/main/java/com/angcyo/library/LibFileProvider.kt
@@ -0,0 +1,14 @@
+package com.angcyo.library
+
+import androidx.core.content.FileProvider
+
+/**
+ *
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/04/23
+ * Copyright (c) 2020 ShenZhen Wayto Ltd. All rights reserved.
+ */
+class LibFileProvider : FileProvider() {
+
+}
