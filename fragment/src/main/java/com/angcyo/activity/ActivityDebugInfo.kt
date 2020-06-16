9148ddca396f0df884ff604faa490f997091f55c
angcyo <angcyo@126.com>
2020-06-16 15:36:03 +0800

    * fix debug info

--- fragment/src/main/java/com/angcyo/activity/ActivityDebugInfo.kt
+++ fragment/src/main/java/com/angcyo/activity/ActivityDebugInfo.kt
@@ -7,6 +7,7 @@ import android.content.res.Configuration
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.Rect
+import android.os.Build
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.style.ForegroundColorSpan
@@ -47,15 +48,180 @@ fun Activity.showDebugInfoView(
     val contentView = window.findViewById<View>(Window.ID_ANDROID_CONTENT)
 
     val tag = "debug_info_view"
-    val debugTextView = decorView.findViewWithTag<View>(tag)
-
-    if (debugTextView != null) {
-        (decorView as? ViewGroup)?.removeView(debugTextView)
-    }
+    val debugTextView = decorView.findViewWithTag<TextView>(tag)
 
     if (debug && show) {
-        if (decorView is FrameLayout) {
+        val textView = if (debugTextView == null) {
+            val textView = TextView(this)
+            textView.tag = tag
+            textView.textSize = 9f
+            textView.setTextColor(Color.WHITE)
+            val dp2 = 1 * resources.displayMetrics.density
+            val padding = dp2.toInt() * 4
+            textView.setPadding(padding, padding, padding, padding)
+            textView.setShadowLayer(dp2 * 2, dp2, dp2, Color.BLACK)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                textView.elevation = 2 * dp
+            }
 
+            val layoutParams = FrameLayout.LayoutParams(-2, -2)
+            (decorView as ViewGroup).addView(textView, layoutParams)
+
+            textView
+        } else {
+            debugTextView
+        }
+
+        val layoutParams: FrameLayout.LayoutParams =
+            textView.layoutParams as FrameLayout.LayoutParams
+
+        //显示正常模式, 小圆点
+        fun showNormal(textView: TextView, enable: Boolean = true) {
+            textView.layoutParams = textView.layoutParams.apply {
+                if (enable) {
+                    width = 18 * dpi
+                    height = width
+                } else {
+                    width = -2
+                    height = -2
+                }
+            }
+            if (enable) {
+                textView.text = null
+                textView.setBackgroundResource(R.drawable.lib_theme_gradient_circle_solid_shape)
+            } else {
+                textView.background = null
+            }
+        }
+
+        //显示简单文本模式
+        fun showSingleText(textView: TextView) {
+            showNormal(textView, false)
+            textView.text = span {
+                append(this@showDebugInfoView.simpleClassName())
+                    .append(" $taskId")
+                    .appendln()
+                (this@showDebugInfoView as? FragmentActivity)?.supportFragmentManager?.logAllFragment(
+                    _builder,
+                    false,
+                    "\\-"
+                )
+                _builder.safe()
+            }
+        }
+
+        //显示详细文本模式
+        fun showDetailText(textView: TextView) {
+            showNormal(textView, false)
+
+            val displayMetrics = resources.displayMetrics
+            val widthDp: Float = displayMetrics.widthPixels / displayMetrics.density
+            val heightDp: Float = displayMetrics.heightPixels / displayMetrics.density
+
+            // 屏幕尺寸
+            val width = displayMetrics.widthPixels / displayMetrics.xdpi
+            //displayMetrics.heightPixels / displayMetrics.ydpi
+            val height = decorView.measuredHeight / displayMetrics.ydpi
+            val x = width.toDouble().pow(2.0)
+            val y = height.toDouble().pow(2.0)
+            val screenInches = sqrt(x + y)
+
+            textView.text = buildString {
+                appendln(this@showDebugInfoView.javaClass.name)
+                (this@showDebugInfoView as? FragmentActivity)?.supportFragmentManager?.logAllFragment(
+                    this,
+                    true,
+                    "\\-"
+                )
+
+                val statusBarHeight = getStatusBarHeight()
+                val navBarHeight = max(
+                    decorView.measuredWidth - contentView.measuredWidth,
+                    decorView.measuredHeight - contentView.measuredHeight
+                )
+                append("sh:").append(statusBarHeight).append(" ")
+                    .append(statusBarHeight / displayMetrics.density)
+                append(" nh:").append(navBarHeight).append(" ")
+                    .appendln(navBarHeight / displayMetrics.density)
+
+                append("wp:").append(displayMetrics.widthPixels)
+                append(" hp:").appendln(displayMetrics.heightPixels)
+
+                append("decorW:").append(decorView.measuredWidth)
+                append(" decorH:").appendln(decorView.measuredHeight)
+
+                append("contentW:").append(contentView.measuredWidth)
+                append(" contentH:").appendln(contentView.measuredHeight)
+
+                append("wDp:").append(widthDp)
+                append(" hDp:").append(heightDp)
+                append(" dp:").append(displayMetrics.density)
+                append(" sp:").append(displayMetrics.scaledDensity)
+                append(" dpi:").appendln(displayMetrics.densityDpi)
+
+                append("w:").append("%.02f".format(width))
+                append(" h:").append("%.02f".format(height))
+                append(" inches:").appendln("%.02f".format(screenInches))
+
+                val rect = Rect()
+                val point = Point()
+                decorView.getGlobalVisibleRect(rect, point)
+                append(" d:").append(rect)
+                append(" d:").append(point).appendln()
+
+                contentView.getGlobalVisibleRect(rect, point)
+                append(" c:").append(rect)
+                append(" c:").append(point)
+
+                decorView.getWindowVisibleDisplayFrame(rect)
+                appendln()
+                append(" frame:").append(rect)
+            }
+        }
+
+        layoutParams.gravity = gravity
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+            && decorView.bottom > contentView.bottom
+        ) { //显示了导航栏
+            val resources = resources
+            val resourceId =
+                resources.getIdentifier("navigation_bar_height", "dimen", "android")
+            var navBarHeight = 0
+            if (resourceId > 0) {
+                navBarHeight = resources.getDimensionPixelSize(resourceId)
+            }
+            layoutParams.bottomMargin = navBarHeight
+        }
+        textView.layoutParams = layoutParams
+
+        //默认显示正常模式
+        showNormal(textView)
+
+        textView.setOnClickListener {
+            if (it.isSelected) {
+                //到达最大模式后
+                showNormal(textView)
+                it.isSelected = false
+            } else {
+                showSingleText(textView)
+                it.isSelected = true
+            }
+        }
+
+        textView.setOnLongClickListener {
+            textView.text.copy(textView.context)
+            it.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
+            showDetailText(textView)
+            it.isSelected = true
+            true
+        }
+
+        textView.onDoubleTap {
+            showDebugInfoView(false, debug, gravity)
+            false
+        }
+
+        if (decorView is FrameLayout) {
             if (this is FragmentActivity) {
                 if (decorView.tag is FragmentManager.FragmentLifecycleCallbacks) {
 
@@ -70,120 +236,16 @@ fun Activity.showDebugInfoView(
                     decorView.tag = callback
                 }
             }
-
-            val textView = TextView(this)
-            textView.tag = tag
-            textView.textSize = 9f
-            textView.setTextColor(Color.WHITE)
-            val dp2 = 1 * resources.displayMetrics.density
-            val padding = dp2.toInt() * 4
-            textView.setPadding(padding, padding, padding, padding)
-            textView.setShadowLayer(dp2 * 2, dp2, dp2, Color.BLACK)
-
-            textView.text = span {
-                append(this@showDebugInfoView.simpleClassName())
-                    .append(" $taskId")
-                    .appendln()
-                (this@showDebugInfoView as? FragmentActivity)?.supportFragmentManager?.logAllFragment(
-                    _builder,
-                    false,
-                    "\\-"
-                )
-                _builder.safe()
-            }
-
-            val layoutParams = FrameLayout.LayoutParams(-2, -2)
-            layoutParams.gravity = gravity
-            if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
-                && decorView.getBottom() > contentView.bottom
-            ) { //显示了导航栏
-                val resources = resources
-                val resourceId =
-                    resources.getIdentifier("navigation_bar_height", "dimen", "android")
-                var navBarHeight = 0
-                if (resourceId > 0) {
-                    navBarHeight = resources.getDimensionPixelSize(resourceId)
-                }
-                layoutParams.bottomMargin = navBarHeight
-            }
-            (decorView as ViewGroup).addView(textView, layoutParams)
-
-            textView.setOnClickListener {
-
-                val displayMetrics = resources.displayMetrics
-                val widthDp: Float = displayMetrics.widthPixels / displayMetrics.density
-                val heightDp: Float = displayMetrics.heightPixels / displayMetrics.density
-
-                // 屏幕尺寸
-                val width = displayMetrics.widthPixels / displayMetrics.xdpi
-                //displayMetrics.heightPixels / displayMetrics.ydpi
-                val height = decorView.measuredHeight / displayMetrics.ydpi
-                val x = width.toDouble().pow(2.0)
-                val y = height.toDouble().pow(2.0)
-                val screenInches = sqrt(x + y)
-
-                textView.text = buildString {
-                    appendln(this@showDebugInfoView.javaClass.name)
-                    (this@showDebugInfoView as? FragmentActivity)?.supportFragmentManager?.logAllFragment(
-                        this,
-                        true,
-                        "\\-"
-                    )
-
-                    val statusBarHeight = getStatusBarHeight()
-                    val navBarHeight = max(
-                        decorView.measuredWidth - contentView.measuredWidth,
-                        decorView.measuredHeight - contentView.measuredHeight
-                    )
-                    append("sh:").append(statusBarHeight).append(" ")
-                        .append(statusBarHeight / displayMetrics.density)
-                    append(" nh:").append(navBarHeight).append(" ")
-                        .appendln(navBarHeight / displayMetrics.density)
-
-                    append("wp:").append(displayMetrics.widthPixels)
-                    append(" hp:").appendln(displayMetrics.heightPixels)
-
-                    append("decorW:").append(decorView.measuredWidth)
-                    append(" decorH:").appendln(decorView.measuredHeight)
-
-                    append("contentW:").append(contentView.measuredWidth)
-                    append(" contentH:").appendln(contentView.measuredHeight)
-
-                    append("wDp:").append(widthDp)
-                    append(" hDp:").append(heightDp)
-                    append(" dp:").append(displayMetrics.density)
-                    append(" sp:").append(displayMetrics.scaledDensity)
-                    append(" dpi:").appendln(displayMetrics.densityDpi)
-
-                    append("w:").append("%.02f".format(width))
-                    append(" h:").append("%.02f".format(height))
-                    append(" inches:").appendln("%.02f".format(screenInches))
-
-                    val rect = Rect()
-                    val point = Point()
-                    decorView.getGlobalVisibleRect(rect, point)
-                    append(" d:").append(rect)
-                    append(" d:").append(point).appendln()
-
-                    contentView.getGlobalVisibleRect(rect, point)
-                    append(" c:").append(rect)
-                    append(" c:").append(point)
-
-                    decorView.getWindowVisibleDisplayFrame(rect)
-                    appendln()
-                    append(" frame:").append(rect)
-                }
-            }
-
-            textView.setOnLongClickListener {
-                textView.text.copy(textView.context)
-                it.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
-                false
-            }
-
-            textView.onDoubleTap {
-                showDebugInfoView(false, debug, gravity)
-                false
+        }
+    } else {
+        //移除已经存在TextView
+        if (debugTextView != null) {
+            (decorView as? ViewGroup)?.removeView(debugTextView)
+        }
+        if (this is FragmentActivity) {
+            if (decorView.tag is FragmentManager.FragmentLifecycleCallbacks) {
+                supportFragmentManager.unregisterFragmentLifecycleCallbacks(decorView.tag as FragmentManager.FragmentLifecycleCallbacks)
+                decorView.tag = null
             }
         }
     }
