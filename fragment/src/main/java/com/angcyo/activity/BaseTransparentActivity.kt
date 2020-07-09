b06f090557bc81619e1aa05c9ce0fd9c944307b6
angcyo <angcyo@126.com>
2020-07-09 15:36:10 +0800

    * fix base style name

---
+++ fragment/src/main/java/com/angcyo/activity/BaseTransparentActivity.kt
@@ -0,0 +1,12 @@
+package com.angcyo.activity
+
+/**
+ * 透明[Activity], 需要使用主题[@style/BaseTranAppTheme]
+ * Email:angcyo@126.com
+ * @author angcyo
+ * @date 2020/07/09
+ * Copyright (c) 2020 ShenZhen Wayto Ltd. All rights reserved.
+ */
+abstract class BaseTransparentActivity : BaseAppCompatActivity() {
+
+}
