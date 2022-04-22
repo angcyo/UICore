d9924ca5ee563aa04676c8ad77417515f5d9abab
angcyo <angcyo@126.com>
2022-04-22 16:29:39 +0800

    * fix drawable flip render

--- canvas/src/main/java/com/angcyo/canvas/items/renderer/DrawableItemRenderer.kt
+++ canvas/src/main/java/com/angcyo/canvas/items/renderer/DrawableItemRenderer.kt
@@ -1,12 +1,10 @@
 package com.angcyo.canvas.items.renderer
 
 import android.content.res.Resources
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.core.graphics.withMatrix
 import com.angcyo.canvas.CanvasView
 import com.angcyo.canvas.ScalePictureDrawable
 import com.angcyo.canvas.core.CanvasViewBox
@@ -42,16 +40,43 @@ open class DrawableItemRenderer<T : DrawableItem>(canvasViewBox: CanvasViewBox) 
         }
     }
 
+    val flipMatrix = Matrix()
+    val flipRect = RectF()
+
     override fun render(canvas: Canvas) {
         rendererItem?.drawable?.let { drawable ->
             val bounds = getRendererBounds()
-            drawable.setBounds(
-                bounds.left.toInt(),
-                bounds.top.toInt(),
-                bounds.right.toInt(),
-                bounds.bottom.toInt()
-            )
-            drawable.draw(canvas)
+            //需要处理矩形翻转的情况
+            if (drawable is ScalePictureDrawable) {
+                drawable.setBounds(
+                    bounds.left.toInt(),
+                    bounds.top.toInt(),
+                    bounds.right.toInt(),
+                    bounds.bottom.toInt()
+                )
+                drawable.draw(canvas)
+            } else {
+                bounds.adjustFlipRect(flipRect)
+                var sx = 1f
+                var sy = 1f
+                if (getBounds().isFlipHorizontal) {
+                    sx = -1f
+                }
+                if (getBounds().isFlipVertical) {
+                    sy = -1f
+                }
+                flipMatrix.reset()
+                flipMatrix.postScale(sx, sy, flipRect.centerX(), flipRect.centerY())
+                canvas.withMatrix(flipMatrix) {
+                    drawable.setBounds(
+                        flipRect.left.toInt(),
+                        flipRect.top.toInt(),
+                        flipRect.right.toInt(),
+                        flipRect.bottom.toInt()
+                    )
+                    drawable.draw(canvas)
+                }
+            }
         }
     }
 }
