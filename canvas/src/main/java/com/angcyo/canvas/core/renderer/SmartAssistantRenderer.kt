ac9448a189a20900b166fbde2c40d847e7f83f9c
angcyo <angcyo@126.com>
2022-05-13 10:18:23 +0800

    + canvas colors

--- canvas/src/main/java/com/angcyo/canvas/core/renderer/SmartAssistantRenderer.kt
+++ canvas/src/main/java/com/angcyo/canvas/core/renderer/SmartAssistantRenderer.kt
@@ -3,11 +3,12 @@ package com.angcyo.canvas.core.renderer
 import android.graphics.Canvas
 import android.graphics.Paint
 import androidx.core.graphics.withMatrix
+import com.angcyo.canvas.R
 import com.angcyo.canvas.core.ICanvasView
 import com.angcyo.canvas.core.component.SmartAssistant
 import com.angcyo.canvas.utils.createPaint
+import com.angcyo.library.ex._color
 import com.angcyo.library.ex.dp
-import com.angcyo.library.ex.toColorInt
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -16,7 +17,7 @@ import com.angcyo.library.ex.toColorInt
 class SmartAssistantRenderer(val smartAssistant: SmartAssistant, canvasView: ICanvasView) :
     BaseRenderer(canvasView) {
 
-    val paint = createPaint("#8200f1".toColorInt(), Paint.Style.FILL)
+    val paint = createPaint(_color(R.color.canvas_assistant), Paint.Style.FILL)
 
     /**提示线的宽度*/
     var strokeWidth = 1 * dp
