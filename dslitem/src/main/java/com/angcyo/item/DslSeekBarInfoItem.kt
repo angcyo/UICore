f5ca58d9d18dbbe78a459c38b2fce617f1ad5ec8
angcyo <angcyo@126.com>
2022-05-16 19:35:46 +0800

    + seek bar item

---
+++ dslitem/src/main/java/com/angcyo/item/DslSeekBarInfoItem.kt
@@ -0,0 +1,60 @@
+package com.angcyo.item
+
+import com.angcyo.dsladapter.DslAdapterItem
+import com.angcyo.widget.DslViewHolder
+import com.angcyo.widget.progress.DslProgressBar
+import com.angcyo.widget.progress.DslSeekBar
+
+/**
+ * [com.angcyo.widget.progress.DslSeekBar] 滑动
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/05/16
+ */
+open class DslSeekBarInfoItem : DslBaseInfoItem() {
+
+    /**[0-100]*/
+    var itemSeekProgress: Int = 0
+
+    /**是否显示进度文本*/
+    var itemShowProgressText: Boolean = false
+
+    /**进度文本格式化*/
+    var itemProgressTextFormatAction: (DslProgressBar) -> String = {
+        it.progressTextFormat.format("${(it._progressFraction * 100).toInt()}")
+    }
+
+    /**进度改变回调,
+     * [value] 进度值
+     * [fraction] 进度比例
+     * [fromUser] 是否是用户触发*/
+    var itemSeekChanged: (value: Int, fraction: Float, fromUser: Boolean) -> Unit = { _, _, _ -> }
+
+    /**Touch结束后的回调*/
+    var itemSeekTouchEnd: (value: Int, fraction: Float) -> Unit = { _, _ -> }
+
+    init {
+        itemExtendLayoutId = R.layout.dsl_extent_seek_item
+    }
+
+    override fun onItemBind(
+        itemHolder: DslViewHolder,
+        itemPosition: Int,
+        adapterItem: DslAdapterItem,
+        payloads: List<Any>
+    ) {
+        super.onItemBind(itemHolder, itemPosition, adapterItem, payloads)
+
+        itemHolder.v<DslSeekBar>(R.id.lib_seek_view)?.apply {
+            showProgressText = itemShowProgressText
+            progressTextFormatAction = itemProgressTextFormatAction
+
+
+            setProgress(itemSeekProgress, progressValue, -1)
+            config {
+                onSeekChanged = itemSeekChanged
+                onSeekTouchEnd = itemSeekTouchEnd
+            }
+        }
+    }
+
+}
