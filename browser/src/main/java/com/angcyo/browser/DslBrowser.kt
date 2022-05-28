d7a4bd9091c1cc504fe5b59ac89197cb9c77fa7a
angcyo <angcyo@126.com>
2022-05-28 14:29:17 +0800

    * fix base

--- browser/src/main/java/com/angcyo/browser/DslBrowser.kt
+++ browser/src/main/java/com/angcyo/browser/DslBrowser.kt
@@ -16,10 +16,34 @@ object DslBrowser {
 
 /**需要安装支持自定义标签的浏览器, (可能需要设置默认应用)
  * 否则会使用默认的WebView加载*/
-fun String.openCustomTab(activity: Activity) {
+fun String.openCustomTab(activity: Activity, block: CustomTabsIntent.Builder.() -> Unit = {}) {
     val url: String = this
-    val customTabsIntent = CustomTabsIntent.Builder().build()
+    val customTabsIntent = CustomTabsIntent.Builder().apply {
+        //自定义ui
+
+        /*val defaultColors = CustomTabColorSchemeParams.Builder()
+            .setToolbarColor(_color(R.color.colorAccent))
+            .setSecondaryToolbarColor(_color(R.color.colorPrimaryDark))
+            .build()
+        setDefaultColorSchemeParams(defaultColors)*/
+
+        //setActionButton(icon, actionLabel, pendingIntent)
+        //addMenuItem(menuItemTitle, menuItemPendingIntent)
+
+        //setShowTitle(mShowTitleCheckBox.isChecked())
+        //setUrlBarHidingEnabled(mAutoHideAppBarCheckbox.isChecked())
+
+        //setCloseButtonIcon(toBitmap(getDrawable(R.drawable.ic_arrow_back)))
+
+        //setStartAnimations(activity, android.R.anim.slide_in_right, android.R.anim.slide_out_left)
+        //setExitAnimations(activity, android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+
+        block()
+    }.build()
     CustomTabActivityHelper.openCustomTab(
-        activity, customTabsIntent, Uri.parse(url), WebviewFallback()
+        activity,
+        customTabsIntent,
+        Uri.parse(url),
+        WebviewFallback()
     )
 }
