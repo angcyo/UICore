7203045784d56e70f3b9cbc22b8fe96aef7ad000
angcyo <angcyo@126.com>
2022-05-31 16:12:52 +0800

    * fix path to gcode

--- canvas/src/main/java/com/angcyo/canvas/utils/EngraveHelper.kt
+++ canvas/src/main/java/com/angcyo/canvas/utils/EngraveHelper.kt
@@ -3,6 +3,7 @@ package com.angcyo.canvas.utils
 import android.graphics.Matrix
 import android.graphics.Path
 import android.graphics.RectF
+import com.angcyo.canvas.core.MmValueUnit
 import com.angcyo.library.ex.eachPath
 import java.io.File
 import kotlin.math.absoluteValue
@@ -21,11 +22,12 @@ object EngraveHelper {
      * [rotate] 路径需要旋转的角度
      * [outputFile] GCode输出路径
      * */
-    fun pathStrokeToGCode(path: Path, bounds: RectF, rotate: Float, outputFile: File) {
+    fun pathStrokeToGCode(path: Path, bounds: RectF, rotate: Float, outputFile: File): File {
         val gap = 10 //如果2点之间的间隙大于此值, 则使用G0指令
         var lastX = Float.MIN_VALUE
         var lastY = Float.MIN_VALUE
 
+        //形状的路径, 用来计算path内的左上角偏移量
         val pathBounds = RectF()
         path.computeBounds(pathBounds, true)
         val targetPath = Path(path)
@@ -33,17 +35,25 @@ object EngraveHelper {
         val matrix = Matrix()
         matrix.postRotate(rotate, bounds.centerX(), bounds.centerY())
 
+        //旋转的支持
         matrix.mapRect(pathBounds, pathBounds)
         targetPath.transform(matrix)
 
         val offsetLeft = bounds.left - min(0f, pathBounds.left)
         val offsetTop = bounds.top - min(0f, pathBounds.top)
 
+        //像素单位转成mm单位
+        val mmValueUnit = MmValueUnit()
+
         var isCloseCnc = false
         outputFile.writer().use { writer ->
             targetPath.eachPath { index, pos ->
-                val x = pos[0] + offsetLeft
-                val y = pos[1] + offsetTop
+                //像素单位, 要转成G21毫米单位
+                val xPixel = pos[0] + offsetLeft
+                val yPixel = pos[1] + offsetTop
+
+                val x = mmValueUnit.convertPixelToValue(xPixel)
+                val y = mmValueUnit.convertPixelToValue(yPixel)
 
                 if (index == 0) {
                     writer.appendLine("G21")
@@ -81,6 +91,8 @@ object EngraveHelper {
             }
             writer.appendLine("G0 X0 Y0")
         }
+
+        return outputFile
     }
 
 }
