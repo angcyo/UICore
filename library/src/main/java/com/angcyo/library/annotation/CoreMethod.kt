cdb90fa94b7d9adcefe33e959c589e44d6bd4ed1
angcyo <angcyo@126.com>
2022-06-21 09:44:48 +0800

    + annotation

---
+++ library/src/main/java/com/angcyo/library/annotation/CoreMethod.kt
@@ -0,0 +1,10 @@
+package com.angcyo.library.annotation
+
+/**
+ * 标识 核心方法
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/06/21
+ */
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class CoreMethod
