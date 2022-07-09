64602b70f332ad9d38660f0179cc163c9820d3ba
angcyo <angcyo@126.com>
2022-07-09 14:18:37 +0800

    + gitee version update

---
+++ download/src/main/java/com/angcyo/download/GiteeVersionUpdate.kt
@@ -0,0 +1,24 @@
+package com.angcyo.download
+
+import android.content.Context
+import com.angcyo.download.version.VersionUpdateBean
+import com.angcyo.download.version.versionUpdate
+import com.angcyo.http.base.bean
+import com.angcyo.http.gitee.Gitee
+import com.angcyo.http.toBean
+
+/**
+ * Gitee版本更新助手
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/07/09
+ */
+
+/**gitee版本更新检查*/
+fun Context.giteeVersionUpdate(versionJson: String = "version.json") {
+    val json = versionJson
+    Gitee.get(json) { data, error ->
+        data?.toBean<VersionUpdateBean>(bean(VersionUpdateBean::class.java))?.let {
+            versionUpdate(it)
+        }
+    }
+}
