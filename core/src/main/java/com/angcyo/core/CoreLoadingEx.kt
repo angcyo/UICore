34c2bb96ccf5dc76e8b636aa83d35c21e5d7aa96
angcyo <angcyo@126.com>
2022-07-09 13:43:10 +0800

    + loading layout

---
+++ core/src/main/java/com/angcyo/core/CoreLoadingEx.kt
@@ -0,0 +1,105 @@
+package com.angcyo.core
+
+import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import androidx.activity.result.ActivityResultCaller
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import com.angcyo.coroutine.launchLifecycle
+import com.angcyo.coroutine.withBlock
+import com.angcyo.dialog.hideLoading
+import com.angcyo.dialog.loading2
+import com.angcyo.drawable.loading.TGStrokeLoadingDrawable
+import com.angcyo.library.L
+import com.angcyo.library.ex.dp
+import com.angcyo.library.ex.setBgDrawable
+import com.angcyo.library.ex.toColorInt
+import com.angcyo.library.toastQQ
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/07/09
+ */
+
+/**异步加载, 带loading dialog*/
+fun <T> LifecycleOwner.loadingAsyncTg(block: () -> T?, action: (T?) -> Unit) {
+    val context = this
+    if (context is ActivityResultCaller) {
+        context.tgStrokeLoading { cancel, loadEnd ->
+            context.launchLifecycle {
+                val result = withBlock { block() }
+                action(result)
+                loadEnd(result, null)
+            }
+        }
+    } else {
+        L.w("context is not ActivityResultCaller!")
+    }
+}
+
+/**
+ * TGStrokeLoadingDrawable 加载样式的loading
+ * [cancel] 是否允许被取消*/
+fun ActivityResultCaller.tgStrokeLoading(
+    cancel: Boolean = false,
+    showErrorToast: Boolean = false,
+    action: (cancel: AtomicBoolean, loadEnd: (data: Any?, error: Throwable?) -> Unit) -> Unit
+): Dialog? {
+    return try {
+        val activity = when (this) {
+            is Fragment -> activity
+            is Activity -> this
+            is Context -> this
+            else -> null
+        } ?: return null
+        activity.tgStrokeLoading2(cancel, showErrorToast, action)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return null
+    }
+}
+
+/**扩展的对象不一样
+ * [Context]*/
+fun Context.tgStrokeLoading2(
+    cancel: Boolean = false,
+    showErrorToast: Boolean = false,
+    action: (cancel: AtomicBoolean, loadEnd: (data: Any?, error: Throwable?) -> Unit) -> Unit
+): Dialog? {
+    val isCancel = AtomicBoolean(false)
+    val dialog = loading2(layoutId = R.layout.lib_tg_stroke_loading_layout, config = {
+        cancelable = cancel
+        onDialogInitListener = { dialog, dialogViewHolder ->
+            val loadingDrawable = TGStrokeLoadingDrawable().apply {
+                loadingOffset = 6 * dp
+                loadingWidth = 6 * dp
+                indeterminateSweepAngle = 1f
+                loadingBgColor = "#ffffff".toColorInt()
+                loadingColor = loadingBgColor
+            }
+            dialogViewHolder.view(R.id.lib_loading_view)?.setBgDrawable(loadingDrawable)
+        }
+    }) { dialog ->
+        isCancel.set(true)
+        action(isCancel) { _, _ ->
+            //no op
+        }
+    }
+
+    isCancel.set(false)
+    action(isCancel) { data, error ->
+        if (error != null) {
+            //失败
+            if (showErrorToast) {
+                toastQQ(error.message)
+            }
+            hideLoading(error.message)
+        } else {
+            hideLoading()
+        }
+    }
+
+    return dialog
+}
