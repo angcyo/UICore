168c68b42d00d49d3f9ddbf78b6e70de42490981
angcyo <angcyo@126.com>
2022-07-13 09:26:27 +0800

    * fix lifecycle ex

--- viewmodel/src/main/java/com/angcyo/lifecycle/LifecycleEx.kt
+++ viewmodel/src/main/java/com/angcyo/lifecycle/LifecycleEx.kt
@@ -2,9 +2,13 @@ package com.angcyo.lifecycle
 
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
 /**
+ * [androidx.lifecycle.LifecycleObserver]
+ * [androidx.lifecycle.DefaultLifecycleObserver]
+ * [androidx.lifecycle.LifecycleEventObserver]
  *
  * Email:angcyo@126.com
  * @author angcyo
@@ -14,39 +18,80 @@ import androidx.lifecycle.LifecycleOwner
 
 /**生命周期快速监听
  * [action] 返回true, 表示调用完之后移除观察*/
-fun Lifecycle.on(event: Lifecycle.Event, action: () -> Boolean): LifecycleEventObserver {
-
-    val observer = object : LifecycleEventObserver {
-        override fun onStateChanged(source: LifecycleOwner, en: Lifecycle.Event) {
-            if (en == event) {
-                if (action()) {
-                    removeObserver(this)
-                }
+fun Lifecycle.on(
+    event: Lifecycle.Event,
+    forever: Boolean = false,
+    action: () -> Boolean
+): LifecycleEventObserver {
+    return onStateChanged(forever) { source, en, observer ->
+        if (en == event || event == Lifecycle.Event.ON_ANY) {
+            if (action()) {
+                removeObserver(observer)
             }
         }
     }
+}
 
+fun Lifecycle.onDestroy(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_DESTROY, forever, action)
+
+fun Lifecycle.onPause(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_PAUSE, forever, action)
+
+fun Lifecycle.onCreate(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_CREATE, forever, action)
+
+fun Lifecycle.onStop(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_STOP, forever, action)
+
+fun Lifecycle.onStart(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_START, forever, action)
+
+fun Lifecycle.onAny(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_ANY, forever, action)
+
+/**监听生命周期改变*/
+fun Lifecycle.onStateChanged(
+    forever: Boolean = false,
+    action: (source: LifecycleOwner, event: Lifecycle.Event, observer: LifecycleObserver) -> Unit
+): LifecycleEventObserver {
+    val observer = object : LifecycleEventObserver {
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            action(source, event, this)
+            if (!forever && event == Lifecycle.Event.ON_DESTROY) {
+                //销毁之后, 自动移除
+                removeObserver(this)
+            }
+        }
+    }
     addObserver(observer)
-
     return observer
 }
 
-fun Lifecycle.onDestroy(action: () -> Boolean) = on(Lifecycle.Event.ON_DESTROY, action)
-fun Lifecycle.onPause(action: () -> Boolean) = on(Lifecycle.Event.ON_PAUSE, action)
-fun Lifecycle.onCreate(action: () -> Boolean) = on(Lifecycle.Event.ON_CREATE, action)
-fun Lifecycle.onStop(action: () -> Boolean) = on(Lifecycle.Event.ON_STOP, action)
-fun Lifecycle.onStart(action: () -> Boolean) = on(Lifecycle.Event.ON_START, action)
-fun Lifecycle.onAny(action: () -> Boolean) = on(Lifecycle.Event.ON_ANY, action)
-
 /*-----------------------------------LifecycleOwner------------------------------------*/
 
-fun LifecycleOwner.on(event: Lifecycle.Event, action: () -> Boolean): LifecycleEventObserver {
-    return lifecycle.on(event, action)
+fun LifecycleOwner.on(
+    event: Lifecycle.Event,
+    forever: Boolean = false,
+    action: () -> Boolean
+): LifecycleEventObserver {
+    return lifecycle.on(event, forever, action)
 }
 
-fun LifecycleOwner.onDestroy(action: () -> Boolean) = on(Lifecycle.Event.ON_DESTROY, action)
-fun LifecycleOwner.onPause(action: () -> Boolean) = on(Lifecycle.Event.ON_PAUSE, action)
-fun LifecycleOwner.onCreate(action: () -> Boolean) = on(Lifecycle.Event.ON_CREATE, action)
-fun LifecycleOwner.onStop(action: () -> Boolean) = on(Lifecycle.Event.ON_STOP, action)
-fun LifecycleOwner.onStart(action: () -> Boolean) = on(Lifecycle.Event.ON_START, action)
-fun LifecycleOwner.onAny(action: () -> Boolean) = on(Lifecycle.Event.ON_ANY, action)
+fun LifecycleOwner.onDestroy(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_DESTROY, forever, action)
+
+fun LifecycleOwner.onPause(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_PAUSE, forever, action)
+
+fun LifecycleOwner.onCreate(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_CREATE, forever, action)
+
+fun LifecycleOwner.onStop(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_STOP, forever, action)
+
+fun LifecycleOwner.onStart(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_START, forever, action)
+
+fun LifecycleOwner.onAny(forever: Boolean = false, action: () -> Boolean) =
+    on(Lifecycle.Event.ON_ANY, forever, action)
