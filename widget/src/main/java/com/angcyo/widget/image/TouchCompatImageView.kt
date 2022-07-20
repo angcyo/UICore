1bca0beccc8675464a2a1294728d3e4c318bbf69
angcyo <angcyo@126.com>
2022-07-20 08:53:30 +0800

    * fix base

--- widget/src/main/java/com/angcyo/widget/image/TouchCompatImageView.kt
+++ widget/src/main/java/com/angcyo/widget/image/TouchCompatImageView.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 
 /**
+ * 单独回调[onTouchEvent]事件
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @since 2022/06/01
  */
