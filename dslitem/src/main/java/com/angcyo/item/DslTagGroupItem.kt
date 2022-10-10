e92c7970b2765514f68549a39900042bdfebe5f5
angcyo <angcyo@126.com>
2022-10-10 14:42:36 +0800

    + dsl tag group item

---
+++ dslitem/src/main/java/com/angcyo/item/DslTagGroupItem.kt
@@ -0,0 +1,56 @@
+package com.angcyo.item
+
+import com.angcyo.dsladapter.DslAdapterItem
+import com.angcyo.item.data.LabelDesData
+import com.angcyo.library.annotation.DSL
+import com.angcyo.widget.DslViewHolder
+import com.angcyo.widget.base.dslViewHolder
+import com.angcyo.widget.base.resetChild
+import com.angcyo.widget.flow
+
+/**
+ * 存放一堆 [label:des] 描述信息的item
+ * [com.angcyo.item.DslSolidTagItem]
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/10/10
+ */
+open class DslTagGroupItem : DslAdapterItem() {
+
+    /**数据存放*/
+    var itemLabelDesList: List<LabelDesData>? = null
+
+    /**填充的布局id*/
+    var itemTagLayoutId = R.layout.dsl_solid_tag_item
+
+    init {
+        itemLayoutId = R.layout.dsl_tag_group_item
+    }
+
+    override fun onItemBind(
+        itemHolder: DslViewHolder,
+        itemPosition: Int,
+        adapterItem: DslAdapterItem,
+        payloads: List<Any>
+    ) {
+        super.onItemBind(itemHolder, itemPosition, adapterItem, payloads)
+
+        initLabelDesList()
+        itemHolder.flow(R.id.lib_flow_layout)
+            ?.resetChild(itemLabelDesList, itemTagLayoutId) { itemView, item, itemIndex ->
+                val viewHolder = itemView.dslViewHolder()
+                viewHolder.tv(R.id.lib_label_view)?.text = item.label
+                viewHolder.tv(R.id.lib_des_view)?.text = item.des
+            }
+    }
+
+    /**初始化数据*/
+    open fun initLabelDesList() {
+    }
+
+    @DSL
+    fun renderLabelDesList(action: MutableList<LabelDesData>.() -> Unit) {
+        val list = mutableListOf<LabelDesData>()
+        list.action()
+        itemLabelDesList = list
+    }
+}
---
+++ dslitem/src/main/java/com/angcyo/item/data/LabelDesData.kt
@@ -0,0 +1,11 @@
+package com.angcyo.item.data
+
+/**
+ * [label:des] 结构的信息
+ * @author <a href="mailto:angcyo@126.com">angcyo</a>
+ * @since 2022/07/20
+ */
+data class LabelDesData(
+    val label: CharSequence? = null,
+    val des: CharSequence? = null,
+)
