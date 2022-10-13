cab767cbacc16fad0f8471f8c7c7ef0ea5c012ce
angcyo <angcyo@126.com>
2022-10-13 11:34:35 +0800

    * fix base

--- download/src/main/java/com/angcyo/download/GiteeVersionUpdate.kt
+++ download/src/main/java/com/angcyo/download/GiteeVersionUpdate.kt
@@ -16,11 +16,11 @@ import com.angcyo.http.toBean
 /**gitee版本更新检查
  * 在[com.angcyo.http.gitee.Gitee.getBASE]目录下存放一个[version.json]即可获取更新信息
  * */
-fun Context.giteeVersionUpdate(versionJson: String = "version.json") {
+fun Context.giteeVersionUpdate(versionJson: String = "version.json", debug: Boolean = false) {
     val json = versionJson
     Gitee.get(json) { data, error ->
         data?.toBean<VersionUpdateBean>(bean(VersionUpdateBean::class.java))?.let {
-            versionUpdate(it)
+            versionUpdate(it, debug = debug)
         }
     }
 }
