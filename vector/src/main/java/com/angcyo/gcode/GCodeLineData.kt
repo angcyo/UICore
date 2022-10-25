9720f4980145f58546b5ae58204ff34af0f01f3d
angcyo <angcyo@126.com>
2022-10-25 09:20:35 +0800

    * fix base

--- vector/src/main/java/com/angcyo/gcode/GCodeLineData.kt
+++ vector/src/main/java/com/angcyo/gcode/GCodeLineData.kt
@@ -59,12 +59,14 @@ fun GCodeLineData.isSpindleOn(def: Boolean): Boolean {
     val firstCmd = cmdList.firstOrNull()
     val firstCmdString = firstCmd?.cmd
     if (firstCmdString?.startsWith("M") == true) {
-        var isSpindleOn = true //M05指令:主轴关闭, M03:主轴打卡
+        var isSpindleOn = true //M05指令:主轴关闭, M03:主轴打开
         val number = firstCmd.number.toInt()
-        if (number == 5) {
-            isSpindleOn = false
-        } else if (number == 3) {
-            isSpindleOn = true
+        when (number) {
+            5 -> isSpindleOn = false
+            3 -> isSpindleOn = true
+            4 -> {
+                //自动CNC
+            }
         }
         return isSpindleOn
     }
