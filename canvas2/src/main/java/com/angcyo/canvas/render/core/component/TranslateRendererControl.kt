235ed3c0c09016f17e7fe18228734fd373c852eb
angcyo <angcyo@126.com>
2023-03-13 09:46:02 +0800

    * fix translate control

--- canvas2/src/main/java/com/angcyo/canvas/render/core/component/TranslateRendererControl.kt
+++ canvas2/src/main/java/com/angcyo/canvas/render/core/component/TranslateRendererControl.kt
@@ -19,15 +19,18 @@ class TranslateRendererControl(controlManager: CanvasControlManager) : BaseContr
         super.onTouchEvent(event)
         when (event.actionMasked) {
             MotionEvent.ACTION_MOVE -> {
-                val tx = touchMovePointInside.x - touchDownPointInside.x
-                val ty = touchMovePointInside.y - touchDownPointInside.y
-                if (isControlHappen ||
-                    tx.absoluteValue >= translateThreshold ||
-                    ty.absoluteValue >= translateThreshold
-                ) {
-                    //已经发生过移动, 或者移动距离大于阈值
-                    if (tx != 0f && ty != 0f) {
-                        translate(tx, ty)
+                if (event.pointerCount <= 1) {
+                    //单指才能移动元素
+                    val tx = touchMovePointInside.x - touchDownPointInside.x
+                    val ty = touchMovePointInside.y - touchDownPointInside.y
+                    if (isControlHappen ||
+                        tx.absoluteValue >= translateThreshold ||
+                        ty.absoluteValue >= translateThreshold
+                    ) {
+                        //已经发生过移动, 或者移动距离大于阈值
+                        if (tx != 0f && ty != 0f) {
+                            translate(tx, ty)
+                        }
                     }
                 }
             }
@@ -38,12 +41,14 @@ class TranslateRendererControl(controlManager: CanvasControlManager) : BaseContr
     /**直接将渲染器移动多少距离
      * [tx] [ty] 如果是手指触发的, 那么距离应该是移动的位置减去首次按下时的距离*/
     private fun translate(tx: Float, ty: Float) {
-        L.d("移动元素:tx:$tx ty:$ty")
-        controlRendererInfo?.let {
-            isControlHappen = true
-            controlMatrix.setTranslate(tx, ty)
+        if (handleControl) {
+            L.d("移动元素:tx:$tx ty:$ty")
+            controlRendererInfo?.let {
+                isControlHappen = true
+                controlMatrix.setTranslate(tx, ty)
 
-            applyTranslate(Reason.preview, controlManager.delegate)
+                applyTranslate(Reason.preview, controlManager.delegate)
+            }
         }
     }
 
@@ -51,7 +56,7 @@ class TranslateRendererControl(controlManager: CanvasControlManager) : BaseContr
 
     /**结束平移控制*/
     override fun endControl() {
-        if (isControlHappen) {
+        if (isControlHappen && handleControl) {
             controlRendererInfo?.let {
                 applyTranslate(Reason.user.apply {
                     controlType = CONTROL_TYPE_KEEP_GROUP_PROPERTY or CONTROL_TYPE_TRANSLATE
