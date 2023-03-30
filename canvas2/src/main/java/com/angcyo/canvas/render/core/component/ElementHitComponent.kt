f49750a760e5024306d76f33dbadfcc64f181993
angcyo <angcyo@126.com>
2023-03-30 11:43:57 +0800

    * fix hit gain

--- canvas2/src/main/java/com/angcyo/canvas/render/core/component/ElementHitComponent.kt
+++ canvas2/src/main/java/com/angcyo/canvas/render/core/component/ElementHitComponent.kt
@@ -10,9 +10,11 @@ import com.angcyo.canvas.render.element.IElement
 import com.angcyo.library.annotation.Pixel
 import com.angcyo.library.component.pool.acquireTempRectF
 import com.angcyo.library.component.pool.release
+import com.angcyo.library.ex.computePathBounds
 import com.angcyo.library.ex.contains
 import com.angcyo.library.ex.dp
 import com.angcyo.library.ex.intersect
+import kotlin.math.min
 
 /** 元素的碰撞检测
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -21,7 +23,7 @@ import com.angcyo.library.ex.intersect
 class ElementHitComponent(val element: IElement) : IComponent {
 
     companion object {
-        val elementBoundsPath = Path()
+        internal val elementBoundsPath = Path()
     }
 
     override var isEnableComponent: Boolean = true
@@ -66,31 +68,38 @@ class ElementHitComponent(val element: IElement) : IComponent {
     fun getElementBoundsPath(delegate: CanvasRenderDelegate?, result: Path): Path {
         val property = if (element is BaseElement) element.renderProperty else null
         val renderMatrix = property?.getRenderMatrix(includeRotate = true)
-        val rect = getElementBounds(delegate, acquireTempRectF())
+
+        val rect = acquireTempRectF()
+        rect.set(0f, 0f, property?.width ?: 1f, property?.height ?: 1f)//有可能是线的高度
+
         result.rewind()
         result.addRect(rect, Path.Direction.CW)
         if (renderMatrix != null) {
             result.transform(renderMatrix)
+
+            if (enableHitGain) { //碰撞增益
+                result.computePathBounds(rect)
+
+                val gainScale = min(delegate?.renderViewBox?.getScale() ?: 1f, 1f)
+
+                val hitThresholdSize = hitGainThreshold / gainScale //上下增益10dp
+                val gainSize = hitGainSize / gainScale //上下增益10dp
+
+                if (rect.width() <= hitThresholdSize) {
+                    //目标宽度及其小
+                    rect.left -= gainSize / 2
+                    rect.right = rect.left + gainSize
+                }
+                if (rect.height() <= hitThresholdSize) {
+                    //目标高度及其小
+                    rect.top -= gainSize / 2
+                    rect.bottom = rect.top + gainSize
+                }
+                result.rewind()
+                result.addRect(rect, Path.Direction.CW)
+            }
         }
         rect.release()
         return result
     }
-
-    /**获取元素的矩形, 宽高大小*/
-    @Pixel
-    fun getElementBounds(delegate: CanvasRenderDelegate?, result: RectF): RectF {
-        val renderProperty = if (element is BaseElement) element.renderProperty else null
-        val gainSize = hitGainSize / (delegate?.renderViewBox?.getScale() ?: 1f) //上下增益10dp
-        result.set(0f, 0f, renderProperty?.width ?: 1f, renderProperty?.height ?: 1f)//线的高度
-        if (enableHitGain) {
-            if (result.width() <= hitGainThreshold) {
-                result.inset(-gainSize, 0f)
-            }
-            if (result.height() <= hitGainThreshold) {
-                result.inset(0f, -gainSize)
-            }
-        }
-        return result
-    }
-
 }
