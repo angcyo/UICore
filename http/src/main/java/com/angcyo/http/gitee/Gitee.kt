6eb3ba2f8c9e651c29a0a24a595e734a9eb93433
angcyo <angcyo@126.com>
2023-04-03 14:42:46 +0800

    * fix gitee api

--- http/src/main/java/com/angcyo/http/gitee/Gitee.kt
+++ http/src/main/java/com/angcyo/http/gitee/Gitee.kt
@@ -1,8 +1,12 @@
 package com.angcyo.http.gitee
 
+import com.angcyo.http.BaseRequestConfig
+import com.angcyo.http.RequestBodyConfig
+import com.angcyo.http.RequestConfig
 import com.angcyo.http.base.getBoolean
 import com.angcyo.http.base.getLong
 import com.angcyo.http.base.getString
+import com.angcyo.http.base.readString
 import com.angcyo.http.exception.HttpResponseException
 import com.angcyo.http.interceptor.LogInterceptor
 import com.angcyo.http.rx.doBack
@@ -34,30 +38,15 @@ object Gitee {
     //https://gitcode.net/angcyo/json/-/raw/master/accauto/memory_config.json
     var BASE_BACKUP = "https://gitcode.net/angcyo/json/-/raw/master"
 
-    /**从gitee获取数据*/
+    /**从gitee获取Json数据*/
     fun get(
         json: String,
         end: (data: Response<JsonElement>?, error: Throwable?) -> Unit
     ): Disposable {
-        val url = if (json.isHttpScheme()) {
-            json
-        } else {
-            "$BASE/${json}"
-        }
+        var url = json
         return com.angcyo.http.get {
-            this.url = url
-            query = hashMapOf("time" to nowTime()) //带上时间参数, 避免缓存
-            header = hashMapOf(
-                LogInterceptor.closeLog(false),
-                "Host" to (url.toUri()?.host ?: "gitee.com"),
-                "Referer" to (url.toUri()?.host ?: "gitee.com"),
-                "Cache-Control" to "no-cache",
-                "Cookie" to "gitee-session-n=MEl6NWFQd3RVMjhuL1pjWEJzWWE3MGpXMXFCcXMvRjdEWHl4ZXdQSVhVbFpQdTJtdVdWMVloYmYxWVQ4ZTkvVWFrRnl4WTdtazBpNTZpZkllUnFhTHc9PS0tMUdwM2JSM1R0VnRMNzhmamR6c081dz09--555bc1476152411c5402088a42058307da19dc8e; oschina_new_user=false",
-                "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64"
-            )
-            isSuccessful = {
-                it.isSuccessful
-            }
+            configRequest(url)
+            url = this.url
         }.map {
             if (!it.isSuccessful) {
                 throw IllegalArgumentException("${url}:${it.message()}")
@@ -79,7 +68,65 @@ object Gitee {
         }
     }
 
-    /**检查验证隐逸数据*/
+    /**从gitee获取字符串数据*/
+    fun getString(
+        api: String,
+        end: (data: String?, error: Throwable?) -> Unit
+    ): Disposable {
+        var url = api
+        return com.angcyo.http.get2Body {
+            configRequest(url)
+            url = this.url
+        }.map {
+            if (!it.isSuccessful) {
+                throw IllegalArgumentException("${url}:${it.message()}")
+            }
+            it
+        }.observer {
+            onObserverEnd = { data, error ->
+                if (error is HttpResponseException &&
+                    (error.code == 404 || error.code == 403) &&
+                    BASE != BASE_BACKUP
+                ) {
+                    //替换备用地址, 重新请求
+                    BASE = BASE_BACKUP
+                    getString(api, end)
+                } else {
+                    end(data?.body()?.readString(), error)
+                }
+            }
+        }
+    }
+
+    /**初始化请求的配置*/
+    private fun BaseRequestConfig.configRequest(json: String) {
+        val url = if (json.isHttpScheme()) {
+            json
+        } else {
+            "$BASE/${json}"
+        }
+        this.url = url
+        query = hashMapOf("time" to nowTime()) //带上时间参数, 避免缓存
+        header = hashMapOf(
+            LogInterceptor.closeLog(false),
+            "Host" to (url.toUri()?.host ?: "gitee.com"),
+            "Referer" to (url.toUri()?.host ?: "gitee.com"),
+            "Cache-Control" to "no-cache",
+            "Cookie" to "gitee-session-n=MEl6NWFQd3RVMjhuL1pjWEJzWWE3MGpXMXFCcXMvRjdEWHl4ZXdQSVhVbFpQdTJtdVdWMVloYmYxWVQ4ZTkvVWFrRnl4WTdtazBpNTZpZkllUnFhTHc9PS0tMUdwM2JSM1R0VnRMNzhmamR6c081dz09--555bc1476152411c5402088a42058307da19dc8e; oschina_new_user=false",
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64"
+        )
+        if (this is RequestBodyConfig) {
+            isSuccessful = {
+                it.isSuccessful
+            }
+        } else if (this is RequestConfig) {
+            isSuccessful = {
+                it.isSuccessful
+            }
+        }
+    }
+
+    /**检查验证隐逸数据, 杀掉程序本身*/
     fun checkConceal(json: String, block: (notify: String?) -> Unit) {
         get(json) { data, error ->
             data?.body()?.let {
