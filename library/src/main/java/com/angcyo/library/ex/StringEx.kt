9c82c2204e293e6e56442305fe25b7716568ab67
angcyo <angcyo@126.com>
2023-04-11 11:12:31 +0800

    * fix base

--- library/src/main/java/com/angcyo/library/ex/StringEx.kt
+++ library/src/main/java/com/angcyo/library/ex/StringEx.kt
@@ -1041,5 +1041,8 @@ fun getFileNameFromAttachment(attachment: String?): String? {
 }
 
 /**[com.pixplicity.sharp.Sharp.loadPath]
- * [androidx.core.graphics.PathParser.createPathFromPathData]*/
+ * [androidx.core.graphics.PathParser.createPathFromPathData]
+ *
+ * [M12 21.593c-5.63-5.539-11-10.297-11-14.402 0-3.791 3.068-5.191 5.281-5.191 1.312 0 4.151.501 5.719 4.457 1.59-3.968 4.464-4.447 5.726-4.447 2.54 0 5.274 1.621 5.274 5.181 0 4.069-5.136 8.625-11 14.402]
+ * */
 fun String.toPath(): Path? = PathParser.createPathFromPathData(this)
