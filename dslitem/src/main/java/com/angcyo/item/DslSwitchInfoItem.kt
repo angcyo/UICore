86163a8a709ed45ed5e0a3674c75392da3f3bbae
angcyo <angcyo@126.com>
2023-05-13 19:17:10 +0800

    + new flag

--- dslitem/src/main/java/com/angcyo/item/DslSwitchInfoItem.kt
+++ dslitem/src/main/java/com/angcyo/item/DslSwitchInfoItem.kt
@@ -1,5 +1,8 @@
 package com.angcyo.item
 
+import android.view.Gravity
+import com.angcyo.dsladapter.item.INewFlagItem
+import com.angcyo.dsladapter.item.NewFlagItemConfig
 import com.angcyo.item.style.ISwitchItem
 import com.angcyo.item.style.SwitchItemConfig
 
@@ -10,9 +13,13 @@ import com.angcyo.item.style.SwitchItemConfig
  * @date 2019/08/09
  * Copyright (c) 2019 ShenZhen O&M Cloud Co., Ltd. All rights reserved.
  */
-open class DslSwitchInfoItem : DslBaseInfoItem(), ISwitchItem {
+open class DslSwitchInfoItem : DslBaseInfoItem(), ISwitchItem, INewFlagItem {
 
     override var switchItemConfig: SwitchItemConfig = SwitchItemConfig()
+
+    override var newFlagItemConfig: NewFlagItemConfig = NewFlagItemConfig().apply {
+        itemNewFlagGravity = Gravity.TOP or Gravity.LEFT
+    }
 
     init {
         itemExtendLayoutId = R.layout.dsl_extent_switch_item
