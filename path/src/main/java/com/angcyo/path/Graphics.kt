3b8ec7ddefcf74eca83f4b60116c3fdfa5afb134
angcyo <angcyo@126.com>
2023-05-25 15:54:10 +0800

    * fix graphics

--- path/src/main/java/com/angcyo/path/Graphics.kt
+++ path/src/main/java/com/angcyo/path/Graphics.kt
@@ -7,7 +7,6 @@ import androidx.core.os.BuildCompat
 import androidx.graphics.path.PathIterator
 import androidx.graphics.path.PathSegment
 import androidx.graphics.path.iterator
-import com.angcyo.library.utils.appendValue
 import java.io.File
 import java.io.FileOutputStream
 
@@ -27,67 +26,69 @@ fun Path.toSvgPath(output: File, tolerance: Float = 0.1f): File {
             //M L Q A C Z
             when (segment.type) {
                 PathSegment.Type.Move -> {
-                    writer.appendValue("M")
+                    writer.append("M")
                     val point = segment.points.getOrNull(0)?.apply {
                         lastPoint.set(x, y)
                     }
-                    writer.appendValue((point ?: lastPoint).x)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).y)
+                    writer.append((point ?: lastPoint).x)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).y)
                 }
 
                 PathSegment.Type.Line -> {
-                    writer.appendValue("L")
+                    writer.append("L")
                     val point = segment.points.getOrNull(1)?.apply {
                         lastPoint.set(x, y)
                     }
-                    writer.appendValue((point ?: lastPoint).x)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).y)
+                    writer.append((point ?: lastPoint).x)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).y)
                 }
 
                 PathSegment.Type.Quadratic, PathSegment.Type.Conic -> {
                     //二次曲线 转svg
-                    writer.appendValue("Q")
+                    writer.append("Q")
                     val controlPoint = segment.points.getOrNull(1) //控制点
                     val point = segment.points.getOrNull(1)?.apply {
                         lastPoint.set(x, y)
                     }
-                    writer.appendValue(controlPoint?.x ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue(controlPoint?.y ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).x)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).y)
+                    writer.append(controlPoint?.x ?: 0f)
+                    writer.append(",")
+                    writer.append(controlPoint?.y ?: 0f)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).x)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).y)
                 }
 
                 PathSegment.Type.Cubic -> {
                     //三次曲线 转svg
-                    writer.appendValue("C")
+                    writer.append("C")
                     val controlPoint1 = segment.points.getOrNull(1) //控制点1
                     val controlPoint2 = segment.points.getOrNull(2) //控制点2
                     val point = segment.points.getOrNull(3)?.apply {
                         lastPoint.set(x, y)
                     } //终点
-                    writer.appendValue(controlPoint1?.x ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue(controlPoint1?.y ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue(controlPoint2?.x ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue(controlPoint2?.y ?: 0)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).x)
-                    writer.appendValue(",")
-                    writer.appendValue((point ?: lastPoint).y)
+                    writer.append(controlPoint1?.x ?: 0f)
+                    writer.append(",")
+                    writer.append(controlPoint1?.y ?: 0f)
+                    writer.append(",")
+                    writer.append(controlPoint2?.x ?: 0f)
+                    writer.append(",")
+                    writer.append(controlPoint2?.y ?: 0f)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).x)
+                    writer.append(",")
+                    writer.append((point ?: lastPoint).y)
                 }
 
-                PathSegment.Type.Close -> writer.appendValue("Z")
+                PathSegment.Type.Close -> writer.append("Z")
                 PathSegment.Type.Done -> Unit
-                else -> writer.appendValue(segment)
+                else -> writer.append("$segment")
             }
         }
     }
     return output
 }
+
+fun Appendable.append(value: Float): Appendable = append("$value")
