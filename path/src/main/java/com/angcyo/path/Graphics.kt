4e999dc0df4ae8f29135927186ad425ab642bfb1
angcyo <angcyo@126.com>
2023-05-25 23:16:23 +0800

    * fix to svg / gcode

--- path/src/main/java/com/angcyo/path/Graphics.kt
+++ path/src/main/java/com/angcyo/path/Graphics.kt
@@ -7,6 +7,7 @@ import androidx.core.os.BuildCompat
 import androidx.graphics.path.PathIterator
 import androidx.graphics.path.PathSegment
 import androidx.graphics.path.iterator
+import com.angcyo.library.L
 import java.io.File
 import java.io.FileOutputStream
 
@@ -18,8 +19,8 @@ import java.io.FileOutputStream
 /**将[Path]转换成svg path数据
  * [tolerance] 近似误差值*/
 @OptIn(BuildCompat.PrereleaseSdkCheck::class)
-fun Path.toSvgPath(output: File, tolerance: Float = 0.1f): File {
-    FileOutputStream(output, false).writer().use { writer ->
+fun Path.toSvgPath(output: File, tolerance: Float = 0.1f, append: Boolean = false): File {
+    FileOutputStream(output, append).writer().use { writer ->
         val lastPoint = PointF(0f, 0f)
         //转成对应的SVG path数据
         for (segment in this.iterator(PathIterator.ConicEvaluation.AsQuadratics, tolerance)) {
@@ -84,7 +85,63 @@ fun Path.toSvgPath(output: File, tolerance: Float = 0.1f): File {
 
                 PathSegment.Type.Close -> writer.append("Z")
                 PathSegment.Type.Done -> Unit
-                else -> writer.append("$segment")
+                else -> {
+                    L.w("不支持的数据:$segment")
+                }
+            }
+        }
+    }
+    return output
+}
+
+/**转GCode数据*/
+@OptIn(BuildCompat.PrereleaseSdkCheck::class)
+fun Path.toGCodePath(output: File, tolerance: Float = 0.1f, append: Boolean = false): File {
+    FileOutputStream(output, append).writer().use { writer ->
+        val lastPoint = PointF(0f, 0f)
+        //转成对应的SVG path数据
+        for (segment in this.iterator(PathIterator.ConicEvaluation.AsQuadratics, tolerance)) {
+            //M L Q A C Z
+            when (segment.type) {
+                PathSegment.Type.Move -> {
+                    writer.append("G0")
+                    val point = segment.points.getOrNull(0)?.apply {
+                        lastPoint.set(x, y)
+                    }
+                    writer.append("X${(point ?: lastPoint).x}")
+                    writer.append("Y${(point ?: lastPoint).y}")
+                    writer.appendLine()
+                }
+
+                PathSegment.Type.Line -> {
+                    writer.append("G1")
+                    val point = segment.points.getOrNull(1)?.apply {
+                        lastPoint.set(x, y)
+                    }
+                    writer.append("X${(point ?: lastPoint).x}")
+                    writer.append("Y${(point ?: lastPoint).y}")
+                    writer.appendLine()
+                }
+
+                PathSegment.Type.Quadratic, PathSegment.Type.Conic -> {
+                    //二次曲线 转svg
+                    writer.append("G2")
+                    val startPoint = segment.points.getOrNull(0) //起点
+                    val point = segment.points.getOrNull(1)?.apply {
+                        lastPoint.set(x, y)
+                    }
+                    writer.append("I${(startPoint!!.x + (point ?: lastPoint).x) / 2}")
+                    writer.append("J${(startPoint.y + (point ?: lastPoint).y) / 2}")
+                    writer.append("X${(point ?: lastPoint).x}")
+                    writer.append("Y${(point ?: lastPoint).y}")
+                    writer.appendLine()
+                }
+
+                PathSegment.Type.Close -> writer.append("M2")
+                PathSegment.Type.Done -> Unit
+                else -> {
+                    L.w("不支持的数据:$segment")
+                }
             }
         }
     }
