fc384b0ff4e77e60c0cc0d2f2baead56f14c840c
angcyo <angcyo@126.com>
2023-06-28 17:04:58 +0800

    * fix show rect bounds

--- library/src/main/java/com/angcyo/library/canvas/single/SingleMatrixDelegate.kt
+++ library/src/main/java/com/angcyo/library/canvas/single/SingleMatrixDelegate.kt
@@ -1,13 +1,21 @@
 package com.angcyo.library.canvas.single
 
 import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.RectF
 import android.view.MotionEvent
 import android.view.View
+import com.angcyo.library.canvas.annotation.CanvasInsideCoordinate
 import com.angcyo.library.canvas.core.CanvasTouchManager
 import com.angcyo.library.canvas.core.CanvasViewBox
 import com.angcyo.library.canvas.core.ICanvasView
 import com.angcyo.library.canvas.core.IRendererManager
+import com.angcyo.library.canvas.core.Reason
 import com.angcyo.library.ex.disableParentInterceptTouchEvent
+import com.angcyo.library.ex.dp
+import com.angcyo.library.ex.getScaleX
+import com.angcyo.library.ex.getScaleY
+import kotlin.math.min
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -86,12 +94,110 @@ class SingleMatrixDelegate(val view: View) : ICanvasView {
         return handle
     }
 
-    override fun refresh(just: Boolean) {
-        if (just) {
-            view.invalidate()
-        } else {
-            view.postInvalidate()
+    /**显示指定矩形的范围*/
+    fun showRectBounds(
+        @CanvasInsideCoordinate
+        rect: RectF? = null,
+        margin: Float = 20 * dp,
+        zoomIn: Boolean = true /*自动放大*/,
+        zoomOut: Boolean = true /*自动缩小*/,
+        lockScale: Boolean = true,
+        anim: Boolean = true,
+        offsetRectTop: Boolean = false,
+        offsetX: Float = 0f,
+        offsetY: Float = 0f,
+        finish: (isCancel: Boolean) -> Unit = {}
+    ) {
+        val rect = rect ?: renderManager.getRendererBounds()
+        val renderViewBox = renderViewBox
+        if (!renderViewBox.isCanvasInit) {
+            view.post {
+                showRectBounds(
+                    rect,
+                    margin,
+                    zoomIn,
+                    zoomOut,
+                    lockScale,
+                    anim,
+                    offsetRectTop,
+                    offsetX,
+                    offsetY,
+                    finish
+                )
+            }
+            return
         }
+
+        val contentWidth = renderViewBox.renderBounds.width()
+        val contentHeight = renderViewBox.renderBounds.height()
+
+        val centerX = contentWidth / 2
+        val centerY = contentHeight / 2
+        val originPoint = renderViewBox.getOriginPoint()
+
+        //先将坐标系移动到view的中心
+        val coordinateTranslateX = centerX - originPoint.x
+        val coordinateTranslateY = centerY - originPoint.y
+
+        //再计算目标中心需要偏移的距离量
+        val translateX = coordinateTranslateX - rect.centerX()
+        val translateY = coordinateTranslateY - rect.centerY()
+
+        val matrix = Matrix()
+        //平移
+        matrix.setTranslate(translateX, translateY)
+
+        val width = rect.width() + margin * 2
+        val height = rect.height() + margin * 2
+
+        var scaleX = renderViewBox.renderMatrix.getScaleX()
+        var scaleY = renderViewBox.renderMatrix.getScaleY()
+
+        val visibleWidth = renderViewBox.visibleBoundsInside.width()
+        val visibleHeight = renderViewBox.visibleBoundsInside.height()
+
+        if (zoomOut) {
+            //需要自动缩小
+            if (width > visibleWidth || height > visibleHeight) {
+                //目标的宽高, 大于画布当前可见的宽高
+                scaleX = (contentWidth - margin * 2) / rect.width()
+                scaleY = (contentHeight - margin * 2) / rect.height()
+            }
+        }
+
+        if (zoomIn) {
+            //需要自动放大
+            if (width < visibleWidth || height < visibleHeight) {
+                //目标的宽高, 小于画布当前可见的宽高
+                scaleX = (contentWidth - margin * 2) / rect.width()
+                scaleY = (contentHeight - margin * 2) / rect.height()
+            }
+        }
+
+        if (lockScale) {
+            scaleX = min(scaleX, scaleY)
+            scaleY = scaleX
+        }
+
+        //自动缩小
+        //自动放大
+        matrix.postScale(
+            scaleX,
+            scaleY,
+            centerX,
+            centerY
+        )
+
+        //偏移量的平移
+        matrix.postTranslate(offsetX, offsetY)
+
+        if (offsetRectTop) {
+            val offset = (renderViewBox.renderBounds.height() - rect.height() * scaleY) / 2 - margin
+            matrix.postTranslate(0f, -offset)
+        }
+
+        //更新
+        renderViewBox.changeRenderMatrix(matrix, anim, Reason.user, finish)
     }
 
 }
--- library/src/main/java/com/angcyo/library/canvas/single/SingleMatrixRenderManager.kt
+++ library/src/main/java/com/angcyo/library/canvas/single/SingleMatrixRenderManager.kt
@@ -2,6 +2,7 @@ package com.angcyo.library.canvas.single
 
 import android.graphics.Canvas
 import android.graphics.PointF
+import android.graphics.RectF
 import androidx.core.graphics.withMatrix
 import androidx.core.graphics.withTranslation
 import com.angcyo.library.annotation.CallPoint
@@ -10,6 +11,8 @@ import com.angcyo.library.canvas.core.IRenderElement
 import com.angcyo.library.canvas.core.IRenderInside
 import com.angcyo.library.canvas.core.IRendererManager
 import java.util.concurrent.CopyOnWriteArrayList
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -39,6 +42,27 @@ class SingleMatrixRenderManager(val delegate: SingleMatrixDelegate) : IRendererM
                 }
             }
         }
+    }
+
+    /**所有元素的边界包裹矩形*/
+    fun getRendererBounds(onlySelect: Boolean = true): RectF {
+        var left: Float? = null
+        var top: Float? = null
+        var right: Float? = null
+        var bottom: Float? = null
+        for (renderer in rendererList) {
+            if (renderer is IRenderElement) {
+                if (onlySelect && !renderer.canSelectElement()) {
+                    continue
+                }
+                val bounds = renderer.getRenderBounds()
+                left = if (left == null) bounds.left else min(left, bounds.left)
+                top = if (top == null) bounds.top else min(top, bounds.top)
+                right = if (right == null) bounds.right else max(right, bounds.right)
+                bottom = if (bottom == null) bounds.bottom else max(bottom, bounds.bottom)
+            }
+        }
+        return RectF(left ?: 0f, top ?: 0f, right ?: 0f, bottom ?: 0f)
     }
 
     /**通过相对于画板原点的点[point], 查找画板内部符合条件的渲染器
