a724acb500ebdb7c921d94f09a233d51dc91e2df
angcyo <angcyo@126.com>
2023-09-21 18:27:09 +0800

    * fix night model

--- crop/src/main/java/com/angcyo/crop/ui/dslitem/CropIconItem.kt
+++ crop/src/main/java/com/angcyo/crop/ui/dslitem/CropIconItem.kt
@@ -1,8 +1,14 @@
 package com.angcyo.crop.ui.dslitem
 
+import android.content.res.ColorStateList
+import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
+import com.angcyo.core.component.model.NightModel
+import com.angcyo.core.vmApp
 import com.angcyo.crop.R
 import com.angcyo.dsladapter.DslAdapterItem
+import com.angcyo.library.ex._color
+import com.angcyo.library.ex.setTintList
 import com.angcyo.widget.DslViewHolder
 
 /**
@@ -22,6 +28,10 @@ open class CropIconItem : DslAdapterItem() {
     /**文本*/
     var itemText: CharSequence? = null
 
+    /**被禁用时的图标颜色*/
+    @ColorInt
+    var itemIcoDisableColor: Int = _color(R.color.crop_item_disable)
+
     init {
         itemLayoutId = R.layout.item_crop_icon_layout
     }
@@ -37,6 +47,12 @@ open class CropIconItem : DslAdapterItem() {
         //
         val imageView = itemHolder.img(R.id.lib_image_view)
         imageView?.rotation = itemIcoRotate
+        if (itemEnable) {
+            imageView?.setTintList(null)
+            vmApp<NightModel>().tintImageViewNight(imageView)
+        } else {
+            imageView?.setTintList(ColorStateList.valueOf(itemIcoDisableColor))
+        }
         imageView?.setImageResource(itemIco)
 
         //
