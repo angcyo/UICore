6ebf679db9af495e551dd45c7b52b402fa42100e
angcyo <angcyo@126.com>
2024-03-13 17:22:35 +0800

    * fix note

--- http/src/main/java/com/angcyo/http/tcp/TcpDevice.kt
+++ http/src/main/java/com/angcyo/http/tcp/TcpDevice.kt
@@ -10,11 +10,16 @@ data class TcpDevice(
      * 可以是ip/也可以是域名使用域名时, 不要http, 直接www.xxx.com即可
      * LX2-411EEA.local
      * LP5-42C17A.local
+     *
+     * 如果是http设备, 则应该是: http://LX2-411EEA.local
      * */
     var address: String,
     /**发送的端口*/
     var port: Int,
-    /**设备名称*/
+    /**设备名称, 通过名称判断设备类型
+     * [String.deviceType]
+     * [LaserPeckerHelper]
+     * */
     var deviceName: String? = null,
     /**设备当前的连接状态
      * [com.angcyo.http.tcp.Tcp.CONNECT_STATE_CONNECTING]
