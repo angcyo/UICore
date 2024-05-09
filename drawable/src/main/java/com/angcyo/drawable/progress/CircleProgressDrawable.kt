03aae2c1119e41f3e6cae20e1f7f0dfe11e00253
angcyo <angcyo@126.com>
2024-05-09 19:00:58 +0800

    * fix progress drawable

--- drawable/src/main/java/com/angcyo/drawable/progress/CircleProgressDrawable.kt
+++ drawable/src/main/java/com/angcyo/drawable/progress/CircleProgressDrawable.kt
@@ -1,7 +1,15 @@
 package com.angcyo.drawable.progress
 
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.Shader
+import android.graphics.SweepGradient
 import androidx.core.graphics.withRotation
+import com.angcyo.drawable.R
+import com.angcyo.library.ex._color
 import kotlin.math.min
 
 /**
@@ -17,6 +25,9 @@ class CircleProgressDrawable : BaseValueProgressDrawable() {
     /**进度偏移绘制的角度*/
     var startOffsetAngle: Float = 0f
 
+    /**是否激活重叠补偿*/
+    var enableCover = true
+
     /**有多少度会出现重叠*/
     var coverAngle: Float = 0f
 
@@ -24,8 +35,10 @@ class CircleProgressDrawable : BaseValueProgressDrawable() {
     var tailAngle: Float = 0f
 
     init {
+        /*progressGradientColors =
+            intArrayOf(Color.parseColor("#79B2FF"), Color.parseColor("#437AFF"))*/
         progressGradientColors =
-            intArrayOf(Color.parseColor("#79B2FF"), Color.parseColor("#437AFF"))
+            intArrayOf(_color(R.color.colorPrimary), _color(R.color.colorPrimaryDark))
         backgroundGradientColors =
             intArrayOf(Color.parseColor("#B0B8CB"), Color.parseColor("#B0B8CB"))
         currentProgressValue = 50
@@ -46,6 +59,21 @@ class CircleProgressDrawable : BaseValueProgressDrawable() {
         startDegrees = -90 - startOffsetAngle
         //补偿
         coverAngle = startOffsetAngle * 2
+    }
+
+    /**更新进度渐变的颜色[progressGradientColors]*/
+    fun updateProgressGradientColors(colors: IntArray) {
+        progressGradientColors = if (colors.size == 1) {
+            intArrayOf(colors[0], colors[0])
+        } else {
+            colors
+        }
+        _progressShader = SweepGradient(
+            bounds.centerX().toFloat(),
+            bounds.centerY().toFloat(),
+            progressGradientColors,
+            null
+        )
     }
 
     override fun setBounds(left: Int, top: Int, right: Int, bottom: Int) {
@@ -94,22 +122,28 @@ class CircleProgressDrawable : BaseValueProgressDrawable() {
             _tempRect.set(rect)
             val inset = (progressWidth / 2).toInt()
             _tempRect.inset(inset, inset)
-            val sweepAngle = (360f - coverAngle - tailAngle) * progressRatio
-            val rotation = coverAngle * progressRatio
 
-            //旋转背景补偿
-            textPaint.shader = null
-            textPaint.color = progressGradientColors[0]
-            canvas.drawArc(
-                _tempRect.left.toFloat(),
-                _tempRect.top.toFloat(),
-                _tempRect.right.toFloat(),
-                _tempRect.bottom.toFloat(),
-                startOffsetAngle,
-                sweepAngle,
-                false,
-                textPaint
-            )
+            var sweepAngle = 360f * progressRatio
+            var rotation = 0f
+
+            if (enableCover) {
+                sweepAngle = (360f - coverAngle - tailAngle) * progressRatio
+                rotation = coverAngle * progressRatio
+
+                //旋转背景补偿
+                textPaint.shader = null
+                textPaint.color = progressGradientColors[0]
+                canvas.drawArc(
+                    _tempRect.left.toFloat(),
+                    _tempRect.top.toFloat(),
+                    _tempRect.right.toFloat(),
+                    _tempRect.bottom.toFloat(),
+                    startOffsetAngle,
+                    sweepAngle,
+                    false,
+                    textPaint
+                )
+            }
 
             //正向旋转
             textPaint.shader = _progressShader
