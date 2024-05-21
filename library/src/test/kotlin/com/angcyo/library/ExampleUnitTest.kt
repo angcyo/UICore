372915c2779809246be3cd664381a4448752b7c9
angcyo <angcyo@126.com>
2024-05-21 18:29:00 +0800

    + test

---
+++ library/src/test/kotlin/com/angcyo/library/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package com.angcyo.library
+
+import com.angcyo.library.ex.bit
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val int = 57
+        println(0 and 0x01)
+        println(int)
+        println(int.bit(1))
+        println(int.bit(2))
+        println(int.bit(3))
+        println("...")
+        println(int shr 0)
+        println((int shr 1) and 0x1)
+    }
+}
