2d9b02f9e7fe020d1aa0426429fd5955918de24f
angcyo <angcyo@126.com>
2025-03-24 18:11:31 +0800

    * fix base

--- widget/src/main/java/com/angcyo/opengl/OpenGLTransformableObject.kt
+++ widget/src/main/java/com/angcyo/opengl/OpenGLTransformableObject.kt
@@ -3,6 +3,11 @@ package com.angcyo.opengl
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @date 2025/03/24
+ *
+ * [android.opengl.Matrix]
+ * [android.opengl.Matrix.setIdentityM] 设置为单位矩阵
+ * [android.opengl.Matrix.multiplyMV] 向量作用一次矩阵[lhsMat]*[rhsVec]
+ *
  */
 open class OpenGLTransformableObject {
 }
