a38924945d162b1eca701cda98f2d81acb09dc0d
angcyo <angcyo@126.com>
2025-03-24 18:01:34 +0800

    * fix render

--- widget/src/main/java/com/angcyo/opengl/OpenGLObject.kt
+++ widget/src/main/java/com/angcyo/opengl/OpenGLObject.kt
@@ -1,5 +1,12 @@
 package com.angcyo.opengl
 
+import android.opengl.GLES20
+import androidx.core.graphics.alpha
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+import com.angcyo.library.annotation.Api
+
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
  * @date 2025/03/24
@@ -28,5 +35,21 @@ open class OpenGLObject : OpenGLTransformableObject() {
     ) {
 
     }
+
+    //region --api--
+
+    /**绘制一个颜色*/
+    @Api
+    fun drawColor(color: Int) {
+        GLES20.glClearColor(
+            color.red / 255f,
+            color.green / 255f,
+            color.blue / 255f,
+            color.alpha / 255f
+        )
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+    }
+
+    //endregion --api--
 
 }
--- widget/src/main/java/com/angcyo/opengl/TestOpenGLObject.kt
+++ widget/src/main/java/com/angcyo/opengl/TestOpenGLObject.kt
@@ -1,11 +1,6 @@
 package com.angcyo.opengl
 
 import android.graphics.Color
-import android.opengl.GLES20
-import androidx.core.graphics.alpha
-import androidx.core.graphics.blue
-import androidx.core.graphics.green
-import androidx.core.graphics.red
 
 /**
  * @author <a href="mailto:angcyo@126.com">angcyo</a>
@@ -14,17 +9,12 @@ import androidx.core.graphics.red
 class TestOpenGLObject : OpenGLObject() {
 
     /**渲染的背景颜色*/
-    var backgroundColor: Int = Color.YELLOW
+    var backgroundColor: Int = Color.MAGENTA
 
     override fun render(vpMatrix: FloatArray, projMatrix: FloatArray?, vMatrix: FloatArray?) {
         super.render(vpMatrix, projMatrix, vMatrix)
         //L.d("")
-        /*GLES20.glClearColor(
-            backgroundColor.red / 255f,
-            backgroundColor.green / 255f,
-            backgroundColor.blue / 255f,
-            backgroundColor.alpha / 255f
-        )*/
-        //GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        drawColor(backgroundColor)
+        //drawColor(Color.TRANSPARENT)
     }
 }
